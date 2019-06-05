7028b05856ef9c3b689958aa066d4e75db74a169
blakeblackshear <blakeb@blakeshome.com>
2019-06-04 21:38:41 -0500

    add a benchmark script

---
+++ benchmark.py
@@ -0,0 +1,20 @@
+import statistics
+import numpy as np
+from edgetpu.detection.engine import DetectionEngine
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = '/frozen_inference_graph.pb'
+
+# Load the edgetpu engine and labels
+engine = DetectionEngine(PATH_TO_CKPT)
+
+frame = np.zeros((300,300,3), np.uint8)
+flattened_frame = np.expand_dims(frame, axis=0).flatten()
+
+detection_times = []
+
+for x in range(0, 1000):
+    objects = engine.DetectWithInputTensor(flattened_frame, threshold=0.1, top_k=3)
+    detection_times.append(engine.get_inference_time())
+
+print("Average inference time: " + str(statistics.mean(detection_times)))
