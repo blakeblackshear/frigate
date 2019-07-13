23833422813a964291061cb3781556d7861d511b
blakeblackshear <blakeb@blakeshome.com>
2019-07-13 07:40:32 -0500

    tweak the label position

--- frigate/util.py
+++ frigate/util.py
@@ -19,8 +19,8 @@ def draw_box_with_label(frame, x_min, y_min, x_max, y_max, label):
     line_height = text_height + size[1]
     # set the text start position
     text_offset_x = x_min
-    text_offset_y = 0 if y_min < line_height else y_min - line_height
+    text_offset_y = 0 if y_min < line_height else y_min - (line_height+8)
     # make the coords of the box with a small padding of two pixels
     textbox_coords = ((text_offset_x, text_offset_y), (text_offset_x + text_width + 2, text_offset_y + line_height))
     cv2.rectangle(frame, textbox_coords[0], textbox_coords[1], color, cv2.FILLED)
-    cv2.putText(frame, label, (text_offset_x, text_offset_y + line_height - 2), font, fontScale=font_scale, color=(0, 0, 0), thickness=2)
+    cv2.putText(frame, label, (text_offset_x, text_offset_y + line_height - 3), font, fontScale=font_scale, color=(0, 0, 0), thickness=2)
