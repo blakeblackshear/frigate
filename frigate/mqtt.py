869edb49a7c700a9a644abec63b273fe7346ab18
blakeblackshear <blakeb@blakeshome.com>
2019-07-14 08:31:21 -0500

    send the best person frame over mqtt for faster updates in homeassistant

--- frigate/mqtt.py
+++ frigate/mqtt.py
@@ -1,13 +1,15 @@
 import json
+import cv2
 import threading
 
 class MqttObjectPublisher(threading.Thread):
-    def __init__(self, client, topic_prefix, objects_parsed, detected_objects):
+    def __init__(self, client, topic_prefix, objects_parsed, detected_objects, best_person_frame):
         threading.Thread.__init__(self)
         self.client = client
         self.topic_prefix = topic_prefix
         self.objects_parsed = objects_parsed
         self._detected_objects = detected_objects
+        self.best_person_frame = best_person_frame
 
     def run(self):
         last_sent_payload = ""
@@ -31,3 +33,9 @@ class MqttObjectPublisher(threading.Thread):
             if new_payload != last_sent_payload:
                 last_sent_payload = new_payload
                 self.client.publish(self.topic_prefix+'/objects', new_payload, retain=False)
+                # send the snapshot over mqtt as well
+                if not self.best_person_frame.best_frame is None:
+                    ret, jpg = cv2.imencode('.jpg', self.best_person_frame.best_frame)
+                    if ret:
+                        jpg_bytes = jpg.tobytes()
+                        self.client.publish(self.topic_prefix+'/snapshot', jpg_bytes, retain=True)
