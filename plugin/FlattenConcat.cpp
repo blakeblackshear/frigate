6e54ea033148660eb1260707d37f4c5716142ac1
Alexander Smirnov <aliaksandr.smirnou@gmail.com>
2020-05-09 17:49:09 +0100

    Nvidia copyright notice

--- plugin/FlattenConcat.cpp
+++ plugin/FlattenConcat.cpp
@@ -1,7 +1,7 @@
 /*
 *  The MIT License
 *  
-*  Copyright (c) 2020 Alexander Smirnov
+*  Copyright (c) 2020 NVIDIA Corporation
 *  
 *  Permission is hereby granted, free of charge, to any person obtaining a copy
 *  of this software and associated documentation files (the "Software"), to deal
