98c0fca0d23f367a871ebac85b6085fa330080a9
Blake Blackshear <blakeb@blakeshome.com>
2020-11-01 10:55:11 -0600

    add capture processes

--- frigate/__main__.py
+++ frigate/__main__.py
@@ -13,7 +13,7 @@ from frigate.http import create_app
 from frigate.models import Event
 from frigate.mqtt import create_mqtt_client
 from frigate.object_processing import TrackedObjectProcessor
-from frigate.video import get_frame_shape, track_camera
+from frigate.video import get_frame_shape, track_camera, get_ffmpeg_input, capture_camera
 
 class FrigateApp():
     def __init__(self):
@@ -36,13 +36,50 @@ class FrigateApp():
         
         self.config = FRIGATE_CONFIG_SCHEMA(config)
 
-        for camera_config in self.config['cameras'].values():
+        for camera_name, camera_config in self.config['cameras'].items():
             if 'width' in camera_config and 'height' in camera_config:
                 frame_shape = (camera_config['height'], camera_config['width'], 3)
             else:
                 frame_shape = get_frame_shape(camera_config['ffmpeg']['input'])
         
             camera_config['frame_shape'] = frame_shape
+
+            ffmpeg = camera_config['ffmpeg']
+            ffmpeg_input = ffmpeg['input']
+            ffmpeg_global_args = ffmpeg.get('global_args', self.config['ffmpeg']['global_args'])
+            ffmpeg_hwaccel_args = ffmpeg.get('hwaccel_args', self.config['ffmpeg']['hwaccel_args'])
+            ffmpeg_input_args = ffmpeg.get('input_args', self.config['ffmpeg']['input_args'])
+            ffmpeg_output_args = ffmpeg.get('output_args', self.config['ffmpeg']['output_args'])
+            if not camera_config.get('fps') is None:
+                ffmpeg_output_args = ["-r", str(camera_config['fps'])] + ffmpeg_output_args
+            if camera_config['save_clips']['enabled']:
+                ffmpeg_output_args = [
+                    "-f",
+                    "segment",
+                    "-segment_time",
+                    "10",
+                    "-segment_format",
+                    "mp4",
+                    "-reset_timestamps",
+                    "1",
+                    "-strftime",
+                    "1",
+                    "-c",
+                    "copy",
+                    "-an",
+                    "-map",
+                    "0",
+                    f"{os.path.join(self.config['save_clips']['cache_dir'], camera_name)}-%Y%m%d%H%M%S.mp4"
+                ] + ffmpeg_output_args
+            ffmpeg_cmd = (['ffmpeg'] +
+                    ffmpeg_global_args +
+                    ffmpeg_hwaccel_args +
+                    ffmpeg_input_args +
+                    ['-i', ffmpeg_input] +
+                    ffmpeg_output_args +
+                    ['pipe:'])
+            
+            camera_config['ffmpeg_cmd'] = ffmpeg_cmd
 
         # TODO: sub in FRIGATE_ENV vars
 
@@ -115,7 +152,13 @@ class FrigateApp():
             print(f"Camera process started for {name}: {camera_process.pid}")
 
     def start_camera_capture_processes(self):
-        pass
+        for name, config in self.config['cameras'].items():
+            capture_process = mp.Process(target=capture_camera, args=(name, config,
+                self.camera_process_info[name]))
+            capture_process.daemon = True
+            self.camera_process_info[name]['capture_process'] = capture_process
+            capture_process.start()
+            print(f"Camera process started for {name}: {capture_process.pid}")
 
     def start_watchdog(self):
         pass
