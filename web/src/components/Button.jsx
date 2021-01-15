ef25f8a31e291a66cec336399bfffe9e8e3e586a
Paul Armstrong <paul@spaceyak.com>
2021-01-15 12:29:30 -0800

    fix(web): mask zone editor to handle object filter masks
    Includes additional handlers for adding/removing masks, as well as click to copy configs
    
    fixes #523

--- web/src/components/Button.jsx
+++ web/src/components/Button.jsx
@@ -2,13 +2,14 @@ import { h } from 'preact';
 
 const noop = () => {};
 
-export default function Button({ children, color, onClick, size }) {
+export default function Button({ children, className, color = 'blue', onClick, size, ...attrs }) {
   return (
     <div
       role="button"
       tabindex="0"
-      className="rounded bg-blue-500 text-white pl-4 pr-4 pt-2 pb-2 font-bold shadow hover:bg-blue-400 hover:shadow-lg cursor-pointer"
+      className={`rounded bg-${color}-500 text-white pl-4 pr-4 pt-2 pb-2 font-bold shadow hover:bg-${color}-400 hover:shadow-lg cursor-pointer ${className}`}
       onClick={onClick || noop}
+      {...attrs}
     >
       {children}
     </div>
