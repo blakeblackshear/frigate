687501313e83ef68054debb4f30972a9a7e7539d
Paul Armstrong <paul@spaceyak.com>
2021-02-05 08:07:24 -0800

    refactor(web): use snowpack-plugin-hash

--- web/snowpack.config.js
+++ web/snowpack.config.js
@@ -5,21 +5,17 @@ module.exports = {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
-  plugins: [
-    '@snowpack/plugin-postcss',
-    '@prefresh/snowpack',
-    [
-      '@snowpack/plugin-webpack',
-      {
-        sourceMap: true,
-      },
-    ],
-  ],
+  plugins: ['@snowpack/plugin-postcss', '@prefresh/snowpack', 'snowpack-plugin-hash'],
   routes: [{ match: 'routes', src: '.*', dest: '/index.html' }],
+  optimize: {
+    bundle: true,
+    minify: true,
+    treeshake: true,
+  },
   packageOptions: {
     sourcemap: false,
   },
   buildOptions: {
-    sourcemap: true,
+    sourcemap: false,
   },
 };
