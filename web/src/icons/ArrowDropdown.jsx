24ec13e36d5d9c92d74fe9138d1845e050c8937c
Paul Armstrong <paul@spaceyak.com>
2021-02-08 13:35:41 -0800

    fix(web): svgs may need explicit height/width in rare cases on linux

--- web/src/icons/ArrowDropdown.jsx
+++ web/src/icons/ArrowDropdown.jsx
@@ -1,9 +1,9 @@
 import { h } from 'preact';
 import { memo } from 'preact/compat';
 
-export function ArrowDropdown() {
+export function ArrowDropdown({ className = '' }) {
   return (
-    <svg className="fill-current" viewBox="0 0 24 24">
+    <svg className={`fill-current ${className}`} viewBox="0 0 24 24">
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M7 10l5 5 5-5z" />
     </svg>
--- web/src/icons/More.jsx
+++ web/src/icons/More.jsx
@@ -1,9 +1,9 @@
 import { h } from 'preact';
 import { memo } from 'preact/compat';
 
-export function More() {
+export function More({ className = '' }) {
   return (
-    <svg className="fill-current" viewBox="0 0 24 24">
+    <svg className={`fill-current ${className}`} viewBox="0 0 24 24">
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
     </svg>
