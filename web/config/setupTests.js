12a0f5d70592a04232f8c44b20fe6b1e1a7e8cb7
Paul Armstrong <paul@spaceyak.com>
2021-02-09 11:35:33 -0800

    test(web): add unit test framework

---
+++ web/config/setupTests.js
@@ -0,0 +1,14 @@
+import 'regenerator-runtime/runtime';
+import '@testing-library/jest-dom/extend-expect';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }),
+});
