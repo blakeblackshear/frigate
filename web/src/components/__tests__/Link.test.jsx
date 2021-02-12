48a8744ffb3ec8cd26ed5f7c3ede27017c7fd3b1
Paul Armstrong <paul@spaceyak.com>
2021-02-12 13:40:36 -0800

    test(web): Link

---
+++ web/src/components/__tests__/Link.test.jsx
@@ -0,0 +1,17 @@
+import { h } from 'preact';
+import Link from '../Link';
+import { render, screen } from '@testing-library/preact';
+
+describe('Link', () => {
+  test('renders a link', async () => {
+    render(<Link href="/tacos">Hello</Link>);
+    expect(screen.queryByText('Hello')).toMatchInlineSnapshot(`
+      <a
+        class="text-blue-500 hover:underline"
+        href="/tacos"
+      >
+        Hello
+      </a>
+    `);
+  });
+});
