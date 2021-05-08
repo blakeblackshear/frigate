069e7025599860717c9347093a024bf9e7cac3d1
Benjamin Dodge <benjamin.dodge@gmail.com>
2021-05-08 15:37:33 -0700

    mqtt: support TLS with self-signed certs

--- frigate/mqtt.py
+++ frigate/mqtt.py
@@ -116,6 +116,10 @@ def create_mqtt_client(config: FrigateConfig, camera_metrics):
             f"{mqtt_config.topic_prefix}/{name}/detect/set", on_detect_command
         )
 
+    if not mqtt_config.tls_ca_certs is None:
+        client.tls_set(mqtt_config.tls_ca_certs)
+    if not mqtt_config.tls_insecure is None:
+        client.tls_insecure_set(mqtt_config.tls_insecure)
     if not mqtt_config.user is None:
         client.username_pw_set(mqtt_config.user, password=mqtt_config.password)
     try:
