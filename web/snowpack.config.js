669247aad912b9a06f459c2178c6fcc67617d163
Blake Blackshear <blakeb@blakeshome.com>
2021-06-11 21:15:21 -0500

    removing testing files

--- web/snowpack.config.js
+++ web/snowpack.config.js
@@ -3,7 +3,6 @@ module.exports = {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
-  exclude: ['**/jsmpeg.*'],
   plugins: ['@snowpack/plugin-postcss', '@prefresh/snowpack', 'snowpack-plugin-hash'],
   routes: [{ match: 'all', src: '(?!.*(.svg|.gif|.json|.jpg|.jpeg|.png|.js)).*', dest: '/index.html' }],
   optimize: {
