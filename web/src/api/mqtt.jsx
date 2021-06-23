f9add57ed4a9736bcd2d7b30072fa8bec8696073
ElMoribond <ElMoribond@users.noreply.github.com>
2021-06-23 15:48:56 +0200

    Add ability to restart
    I restored the original line as it can be misleading.

--- web/src/api/mqtt.jsx
+++ web/src/api/mqtt.jsx
@@ -75,7 +75,7 @@ export function MqttProvider({
 export function useMqtt(watchTopic, publishTopic, defaultValue = null) {
   const { state, ws } = useContext(Mqtt);
 
-  const value = state[watchTopic] || { payload: defaultValue };
+  const value = state[watchTopic] || { payload: null };
 
   const send = useCallback(
     (payload) => {
