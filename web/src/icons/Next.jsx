54616893e5d4cc0e6388fe024d077d0af9fbf13e
JohnMark Sill <johnmark.sill@infinitylabs.io>
2022-01-12 17:01:30 -0600

    chore: added newlines

--- web/src/icons/Next.jsx
+++ web/src/icons/Next.jsx
@@ -4,8 +4,8 @@ import { memo } from 'preact/compat';
 
 export function Next({ className = '' }) {
   return (
-    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-      <path fill="white" d="M4,5V19L11,12M18,5V19H20V5M11,5V19L18,12" />
+    <svg className={`fill-current ${className}`} style="width:24px;height:24px" viewBox="0 0 24 24">
+      <path d="M4,5V19L11,12M18,5V19H20V5M11,5V19L18,12" />
     </svg>
   );
 }
