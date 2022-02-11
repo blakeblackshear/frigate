95ab22d4111f4d09feab2e1a65566896615a4d46
Blake Blackshear <blakeb@blakeshome.com>
2022-02-11 07:30:47 -0600

    bump default stationary_threshold to 10s

--- frigate/config.py
+++ frigate/config.py
@@ -771,7 +771,7 @@ class FrigateConfig(FrigateBaseModel):
                 camera_config.detect.max_disappeared = max_disappeared
 
             # Default stationary_threshold configuration
-            stationary_threshold = camera_config.detect.fps * 5
+            stationary_threshold = camera_config.detect.fps * 10
             if camera_config.detect.stationary_threshold is None:
                 camera_config.detect.stationary_threshold = stationary_threshold
 
