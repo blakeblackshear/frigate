39f889a716cf25830fa8b44a418a083946a0ac57
Nick Mowen <nickmowen213@gmail.com>
2022-02-20 07:58:47 -0700

    Filter cameras when set to not be shown.

--- web/src/Sidebar.jsx
+++ web/src/Sidebar.jsx
@@ -20,7 +20,7 @@ export default function Sidebar() {
           matches ? (
             <Fragment>
               <Separator />
-              {cameras.sort(([aCam, aConf], [bCam, bConf]) => aConf.gui.order === bConf.gui.order ? 0 : (aConf.gui.order > bConf.gui.order ? 1 : -1)).map(([camera]) => (
+              {cameras.filter(([cam, conf]) => conf.gui.show).sort(([aCam, aConf], [bCam, bConf]) => aConf.gui.order === bConf.gui.order ? 0 : (aConf.gui.order > bConf.gui.order ? 1 : -1)).map(([camera]) => (
                 <Destination href={`/cameras/${camera}`} text={camera} />
               ))}
               <Separator />
