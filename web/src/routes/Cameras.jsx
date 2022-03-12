2fe3b2bac4cca9afa57f484a60f3dad928b75705
Nick Mowen <nickmowen213@gmail.com>
2022-03-12 07:40:40 -0700

    Update naming in web

--- web/src/routes/Cameras.jsx
+++ web/src/routes/Cameras.jsx
@@ -17,8 +17,8 @@ export default function Cameras() {
   ) : (
     <div className="grid grid-cols-1 3xl:grid-cols-3 md:grid-cols-2 gap-4 p-2 px-4">
       {Object.entries(config.cameras)
-        .filter(([_, conf]) => conf.gui.show)
-        .sort(([_, aConf], [__, bConf]) => aConf.gui.order === bConf.gui.order ? 0 : (aConf.gui.order > bConf.gui.order ? 1 : -1))
+        .filter(([_, conf]) => conf.ui.show)
+        .sort(([_, aConf], [__, bConf]) => aConf.ui.order === bConf.ui.order ? 0 : (aConf.ui.order > bConf.ui.order ? 1 : -1))
         .map(([camera, conf]) => (
           <Camera key={camera} name={camera} conf={conf} />
         ))}
