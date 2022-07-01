8986dc3f5b96d1d80625c51ea9d04ba0aac68f9b
Nick Mowen <nickmowen213@gmail.com>
2022-07-01 11:40:15 -0600

    Add storage page

---
+++ web/src/routes/Storage.jsx
@@ -0,0 +1,21 @@
+import { h, Fragment } from 'preact';
+import ActivityIndicator from '../components/ActivityIndicator';
+import Button from '../components/Button';
+import Heading from '../components/Heading';
+import Link from '../components/Link';
+import { useMqtt } from '../api/mqtt';
+import useSWR from 'swr';
+import { Table, Tbody, Thead, Tr, Th, Td } from '../components/Table';
+import { useCallback } from 'preact/hooks';
+
+export default function () {
+  const { data: storage } = useSWR('recordings/storage');
+
+  return (
+    <div className="space-y-4 p-2 px-4">
+      <Heading>
+        Storage
+      </Heading>
+    </div>
+  );
+}
