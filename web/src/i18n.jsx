e194499187a0392c76ef192a2b50ccd67c00af77
Nick Mowen <nickmowen213@gmail.com>
2022-08-27 10:21:06 -0600

    Add translation package

---
+++ web/src/i18n.jsx
@@ -0,0 +1,19 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from 'i18next-http-backend';
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(Backend)
+  .use(I18nextBrowserLanguageDetector)
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
+
+export default i18n;
