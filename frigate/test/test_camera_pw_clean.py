448501fe2267872b9f7b354e60dfde45b190b697
Nick Mowen <nickmowen213@gmail.com>
2022-10-09 12:19:54 -0600

    Add tests

---
+++ frigate/test/test_camera_pw_clean.py
@@ -0,0 +1,24 @@
+"""Test camera user and password cleanup."""
+
+import unittest
+
+from frigate.util import clean_camera_user_pass
+
+
+class TestUserPassCleanup(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.rtsp_with_pass = "rtsp://user:password@192.168.0.2:554/live"
+        self.rtsp_no_pass = "rtsp://192.168.0.3/live"
+
+
+    def test_cleanup(self):
+        """Test that user / pass are cleaned up."""
+        clean = clean_camera_user_pass(self.rtsp_with_pass)
+        assert clean != self.rtsp_with_pass
+        assert "user:password" not in self.rtsp_with_pass
+
+    def test_no_cleanup(self):
+        """Test that nothing changes when no user / pass are defined."""
+        clean = clean_camera_user_pass(self.rtsp_no_pass)
+        assert clean == self.rtsp_no_pass
