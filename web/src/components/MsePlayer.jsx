55f3e3aeae384ff7e0c5b3a6bc1303c64c76caa8
Nick Mowen <nickmowen213@gmail.com>
2022-11-08 07:25:35 -0700

    Close stream when pausing

--- web/src/components/MsePlayer.jsx
+++ web/src/components/MsePlayer.jsx
@@ -63,6 +63,11 @@ export default function MsePlayer({ camera, width, height }) {
         }
       }
     };
+
+    video.onpause = () => {
+      ws.close();
+      video.src = null;
+    }
   }, [url]);
 
   return (
