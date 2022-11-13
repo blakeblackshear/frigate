a364f5c3273544c09a9d83fa8e518d304eabd38a
Nick Mowen <nickmowen213@gmail.com>
2022-11-13 09:49:23 -0700

    Fix formatting

--- web/src/routes/CameraMap.jsx
+++ web/src/routes/CameraMap.jsx
@@ -202,12 +202,14 @@ ${Object.keys(objectMaskPoints)
 
       <Card
         content={
-          <p>This tool can help you create masks & zones for your {camera} camera.</p>
-          <ul>
-            <li>Click to add a point.</li>
-            <li>Click and hold on an existing point to move it.</li>
-            <li>Right-Click on an existing point to delete it.</li>
-          </ul>
+          <div>
+            <p>This tool can help you create masks & zones for your {camera} camera.</p>
+            <ul>
+              <li>Click to add a point.</li>
+              <li>Click and hold on an existing point to move it.</li>
+              <li>Right-Click on an existing point to delete it.</li>
+            </ul>
+          </div>
         }
         header="Instructions"
       />
