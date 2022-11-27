68248cc27460049240243b9784ad8955f555e688
Nicolas Mowen <nickmowen213@gmail.com>
2022-11-26 18:18:33 -0700

    Clean http password, clean path in debug config as well (#4519)
    * Clean ffmpeg paths
    
    * Clean http passwords too
    
    * Make check optional

--- frigate/util.py
+++ frigate/util.py
@@ -20,7 +20,7 @@ import numpy as np
 import os
 import psutil
 
-from frigate.const import REGEX_CAMERA_USER_PASS
+from frigate.const import REGEX_HTTP_CAMERA_USER_PASS, REGEX_RTSP_CAMERA_USER_PASS
 
 logger = logging.getLogger(__name__)
 
@@ -718,14 +718,16 @@ def load_labels(path, encoding="utf-8"):
 
 def clean_camera_user_pass(line: str) -> str:
     """Removes user and password from line."""
-    # todo also remove http password like reolink
-    return re.sub(REGEX_CAMERA_USER_PASS, "://*:*@", line)
+    if line.startswith("rtsp://"):
+        return re.sub(REGEX_RTSP_CAMERA_USER_PASS, "://*:*@", line)
+    else:
+        return re.sub(REGEX_HTTP_CAMERA_USER_PASS, "user=*&password=*", line)
 
 
 def escape_special_characters(path: str) -> str:
     """Cleans reserved characters to encodings for ffmpeg."""
     try:
-        found = re.search(REGEX_CAMERA_USER_PASS, path).group(0)[3:-1]
+        found = re.search(REGEX_RTSP_CAMERA_USER_PASS, path).group(0)[3:-1]
         pw = found[(found.index(":") + 1) :]
         return path.replace(pw, urllib.parse.quote_plus(pw))
     except AttributeError:
