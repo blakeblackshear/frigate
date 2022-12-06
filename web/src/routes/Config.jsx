3bdd61c5e14d988dd0b82babeec2ff3c16c1bdd3
Nick Mowen <nickmowen213@gmail.com>
2022-12-05 21:23:37 -0700

    Add ability to copy config

--- web/src/routes/Config.jsx
+++ web/src/routes/Config.jsx
@@ -4,11 +4,16 @@ import CodeEditor from '@uiw/react-textarea-code-editor';
 import ActivityIndicator from '../components/ActivityIndicator';
 import Heading from '../components/Heading';
 import { useState } from 'preact/hooks';
+import Button from '../components/Button';
 
 export default function Config() {
   const { data: config } = useSWR('config/raw');
   const [newCode, setNewCode] = useState(config);
   const [error, setError] = useState();
+
+  const handleCopyConfig = async () => {
+    await window.navigator.clipboard.writeText(newCode);
+  };
 
   if (!config) {
     return <ActivityIndicator />;
@@ -16,7 +21,13 @@ export default function Config() {
 
   return (
     <div className="space-y-4 p-2 px-4">
-      <Heading>Config</Heading>
+      <div className="flex justify-between">
+        <Heading>Config</Heading>
+        <div>
+          <Button className="mx-2" onClick={(e) => handleCopyConfig(e)}>Copy Config</Button>
+          <Button className="mx-2">Save & Restart</Button>
+        </div>
+      </div>
 
       {error && <div className="max-h-20 text-red-500">There is an error</div>}
 
