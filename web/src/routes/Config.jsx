a8c0c64303ecea3c8fb36f80a603d9b6791b46c3
Nick Mowen <nickmowen213@gmail.com>
2022-12-05 21:16:13 -0700

    Add error

--- web/src/routes/Config.jsx
+++ web/src/routes/Config.jsx
@@ -1,4 +1,4 @@
-import { h, Fragment } from 'preact';
+import { h } from 'preact';
 import useSWR from 'swr';
 import CodeEditor from '@uiw/react-textarea-code-editor';
 import ActivityIndicator from '../components/ActivityIndicator';
@@ -8,6 +8,7 @@ import { useState } from 'preact/hooks';
 export default function Config() {
   const { data: config } = useSWR('config/raw');
   const [newCode, setNewCode] = useState(config);
+  const [error, setError] = useState();
 
   if (!config) {
     return <ActivityIndicator />;
@@ -16,6 +17,8 @@ export default function Config() {
   return (
     <div className="space-y-4 p-2 px-4">
       <Heading>Config</Heading>
+
+      {error && <div className="max-h-20 text-red-500">There is an error</div>}
 
       <CodeEditor value={config} language="yaml" onChange={(e) => setNewCode(e.target.value)} />
     </div>
