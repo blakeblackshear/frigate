8c93545e6454241d28f39ade39b2774ce119f031
Nick Mowen <nickmowen213@gmail.com>
2022-12-15 14:21:36 -0700

    Try to set wsdl

--- frigate/ptz.py
+++ frigate/ptz.py
@@ -1,6 +1,7 @@
 """Configure and control camera via onvif."""
 
 import logging
+import site
 
 from enum import Enum
 from onvif import ONVIFCamera
@@ -37,6 +38,7 @@ class OnvifController:
                         cam.onvif.port,
                         cam.onvif.user,
                         cam.onvif.password,
+                        wsdl_dir=site.getsitepackages()[0].replace('dist-packages', 'site-packages') + '/wsdl'
                     ),
                     "init": False,
                     "active": False,
