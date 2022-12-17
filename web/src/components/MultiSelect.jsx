0f7a78876b6545447912bf9cdcbd66d6906859d7
spacebares <57186372+spacebares@users.noreply.github.com>
2022-12-16 18:46:06 -0800

    Show All and Solo selection buttons for MultiSelect.
    Similar to previous behavior of viewing events for single camera with 1 click, or All.
    
    * Fix visual bug with MultiSelect when selecting similar named options.
       eg. options like frontdoor, frontside, backdoor, etc

--- web/src/components/MultiSelect.jsx
+++ web/src/components/MultiSelect.jsx
@@ -3,15 +3,16 @@ import { useRef, useState } from 'preact/hooks';
 import Menu from './Menu';
 import { ArrowDropdown } from '../icons/ArrowDropdown';
 import Heading from './Heading';
+import Button from './Button';
 
-export default function MultiSelect({ className, title, options, selection, onToggle }) {
+export default function MultiSelect({ className, title, options, selection, onToggle, onShowAll, onSolo }) {
 
   const popupRef = useRef(null);
 
   const [state, setState] = useState({
     showMenu: false,
   });
-
+  
   return (
     <div className={`${className} p-2`} ref={popupRef}>
       <div
@@ -23,18 +24,30 @@ export default function MultiSelect({ className, title, options, selection, onTo
       </div>
       {state.showMenu ? (
         <Menu relativeTo={popupRef} onDismiss={() => setState({ showMenu: false })}>
-          <Heading className="p-4 justify-center" size="md">{title}</Heading>
+          <div className="flex flex-wrap gap-2 items-center">
+            <Heading className="p-4 justify-center" size="md">{title}</Heading>
+            <Button className="mx-2" onClick={() => onShowAll() }>
+              Show All
+            </Button>
+          </div>
           {options.map((item) => (
-            <label
-              className={`flex flex-shrink space-x-2 p-1 my-1 min-w-[176px] hover:bg-gray-200 dark:hover:bg-gray-800 dark:hover:text-white cursor-pointer capitalize text-sm`}
-              key={item}>
-              <input
-                className="mx-4 m-0 align-middle"
-                type="checkbox"
-                checked={selection == "all" || selection.indexOf(item) > -1}
-                onChange={() => onToggle(item)} />
-              {item.replaceAll("_", " ")}
-            </label>
+            <div className='flex grow'>
+              <label
+                className={`flex flex-shrink space-x-2 p-1 my-1 min-w-[176px] hover:bg-gray-200 dark:hover:bg-gray-800 dark:hover:text-white cursor-pointer capitalize text-sm`}
+                key={item}>
+                <input
+                  className="mx-4 m-0 align-middle"
+                  type="checkbox"
+                  checked={selection == "all" || selection.split(',').indexOf(item) > -1}
+                  onChange={() => onToggle(item)} />
+                {item.replaceAll("_", " ")}
+              </label>
+              <div className=' justify-right'>
+                <Button className="mx-2" onClick={() => onSolo(item)}>
+                  👀
+                </Button>
+              </div>
+            </div>
           ))}
         </Menu>
       ): null}
