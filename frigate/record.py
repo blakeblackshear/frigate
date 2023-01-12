5dd214d75f22474a2144731e8376837b5790b775
Nick Mowen <nickmowen213@gmail.com>
2023-01-11 21:22:29 -0700

    Formatting

--- frigate/record.py
+++ frigate/record.py
@@ -278,8 +278,7 @@ class RecordingMaintainer(threading.Thread):
 
         directory = os.path.join(
             RECORD_DIR,
-            start_time.astimezone(tz=datetime.timezone.utc)
-            .strftime("%Y-%m-%d/%H"),
+            start_time.astimezone(tz=datetime.timezone.utc).strftime("%Y-%m-%d/%H"),
             camera,
         )
 
