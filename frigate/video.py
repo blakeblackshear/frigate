88b903ba39c09a05f669234866693bfaa7d5ff9a
Nicolas Mowen <nickmowen213@gmail.com>
2023-01-29 16:20:42 -0700

    Restart ffmpeg if process exceeds detect fps by 10 (#5280)
    * Restart ffmpeg if process exceeds detect fps by 10
    
    * Update frigate/video.py
    
    Co-authored-by: Felipe Santos <felipecassiors@gmail.com>
    
    * spelling
    
    ---------
    
    Co-authored-by: Felipe Santos <felipecassiors@gmail.com>

--- frigate/video.py
+++ frigate/video.py
@@ -269,7 +269,20 @@ class CameraWatchdog(threading.Thread):
                     self.logger.info("Waiting for ffmpeg to exit gracefully...")
                     self.ffmpeg_detect_process.communicate(timeout=30)
                 except sp.TimeoutExpired:
-                    self.logger.info("FFmpeg didnt exit. Force killing...")
+                    self.logger.info("FFmpeg did not exit. Force killing...")
+                    self.ffmpeg_detect_process.kill()
+                    self.ffmpeg_detect_process.communicate()
+            elif self.camera_fps.value >= (self.config.detect.fps + 10):
+                self.camera_fps.value = 0
+                self.logger.info(
+                    f"{self.camera_name} exceeded fps limit. Exiting ffmpeg..."
+                )
+                self.ffmpeg_detect_process.terminate()
+                try:
+                    self.logger.info("Waiting for ffmpeg to exit gracefully...")
+                    self.ffmpeg_detect_process.communicate(timeout=30)
+                except sp.TimeoutExpired:
+                    self.logger.info("FFmpeg did not exit. Force killing...")
                     self.ffmpeg_detect_process.kill()
                     self.ffmpeg_detect_process.communicate()
 
