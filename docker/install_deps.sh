27a31e731f671e0a6e79f52c36920b9baf61c629
Blake Blackshear <blakeb@blakeshome.com>
2023-02-11 09:57:15 -0600

    install i965-va-driver-shaders (#5451)

--- docker/install_deps.sh
+++ docker/install_deps.sh
@@ -64,6 +64,9 @@ if [[ "${TARGETARCH}" == "amd64" ]]; then
     apt-get -qq install --no-install-recommends --no-install-suggests -y \
         intel-opencl-icd \
         mesa-va-drivers libva-drm2 intel-media-va-driver-non-free i965-va-driver libmfx1 radeontop intel-gpu-tools
+    # something about this dependency requires it to be installed in a separate call rather than in the line above
+    apt-get -qq install --no-install-recommends --no-install-suggests -y \
+        i965-va-driver-shaders
     rm -f /etc/apt/sources.list.d/debian-testing.list
 fi
 
