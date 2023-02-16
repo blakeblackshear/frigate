bd4b5dbb7dc36ff874db335bccae694f4f259712
Nicolas Mowen <nickmowen213@gmail.com>
2023-02-16 16:27:37 -0700

    wording

--- frigate/http.py
+++ frigate/http.py
@@ -185,7 +185,7 @@ def send_to_plus(id):
         return make_response(jsonify({"success": False, "message": message}), 404)
         
     if event.end_time is None:
-        logger.error(f"Unable to load clean snapshot for in-progress event: {event.id}")
+        logger.error(f"Unable to load clean png for in-progress event: {event.id}")
         return make_response(
             jsonify(
                 {"success": False, "message": "Unable to load clean png for in-progress event"}
