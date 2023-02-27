7323ccaf572f1b6782cb1513cf9b4da568cf54c0
Nick Mowen <nickmowen213@gmail.com>
2023-02-27 07:47:43 -0700

    Formatting

--- frigate/http.py
+++ frigate/http.py
@@ -640,7 +640,9 @@ def events():
             sub_label_clauses.append((Event.sub_label.is_null()))
 
         for label in filtered_sub_labels:
-            sub_label_clauses.append((Event.sub_label.cast("text") == label)) # include exact matches
+            sub_label_clauses.append(
+                (Event.sub_label.cast("text") == label)
+            )  # include exact matches
 
             # include this label when part of a list
             sub_label_clauses.append((Event.sub_label.cast("text") % f"*{label},*"))
