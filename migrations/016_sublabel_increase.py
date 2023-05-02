63ddedc36fb415c5e85cfc075221d40032db9cf3
Sergey Krashevich <svk@svk.su>
2023-05-02 17:19:35 +0300

    black format...

--- migrations/016_sublabel_increase.py
+++ migrations/016_sublabel_increase.py
@@ -3,8 +3,10 @@ from playhouse.migrate import *
 from playhouse.sqlite_ext import *
 from frigate.models import Event
 
+
 def migrate(migrator, database, fake=False, **kwargs):
     migrator.change_columns(Event, sub_label=pw.CharField(max_length=100, null=True))
+
 
 def rollback(migrator, database, fake=False, **kwargs):
     migrator.change_columns(Event, sub_label=pw.CharField(max_length=20, null=True))
