2add675d4242f9c550583fca5a8e66d93e2910e0
Sergey Krashevich <svk@svk.su>
2023-05-05 01:58:59 +0300

    Refactor Process Stats and Bugfixes (#6344)
    * test refactor process stats
    
    * Update util.py
    
    * bugfix
    
    * black formatting
    
    * add missing processes field to StatsTrackingTypes class
    
    * fix python checks and tests...
    
    * use psutil for calcilate cpu utilization on get_cpu_stats
    
    * black...black...black...
    
    * add cpu average
    
    * calculate statiscts for logger process
    
    * add P-ID for other processes in System.jsx
    
    * Apply suggestions from code review
    
    Co-authored-by: Nicolas Mowen <nickmowen213@gmail.com>
    
    * make page beautiful again :)
    
    ---------
    
    Co-authored-by: Nicolas Mowen <nickmowen213@gmail.com>

--- frigate/util.py
+++ frigate/util.py
@@ -799,36 +799,46 @@ def get_cpu_stats() -> dict[str, dict]:
     docker_memlimit = get_docker_memlimit_bytes() / 1024
     total_mem = os.sysconf("SC_PAGE_SIZE") * os.sysconf("SC_PHYS_PAGES") / 1024
 
-    for pid in os.listdir("/proc"):
-        if pid.isdigit():
-            try:
-                with open(f"/proc/{pid}/stat", "r") as f:
-                    stats = f.readline().split()
-                utime = int(stats[13])
-                stime = int(stats[14])
-                cpu_usage = round((utime + stime) / os.sysconf("SC_CLK_TCK"))
+    for process in psutil.process_iter(["pid", "name", "cpu_percent"]):
+        pid = process.info["pid"]
+        try:
+            cpu_percent = process.info["cpu_percent"]
 
-                with open(f"/proc/{pid}/statm", "r") as f:
-                    mem_stats = f.readline().split()
-                mem_res = int(mem_stats[1]) * os.sysconf("SC_PAGE_SIZE") / 1024
+            with open(f"/proc/{pid}/stat", "r") as f:
+                stats = f.readline().split()
+            utime = int(stats[13])
+            stime = int(stats[14])
+            starttime = int(stats[21])
 
-                if docker_memlimit > 0:
-                    mem_pct = round((mem_res / docker_memlimit) * 100, 1)
-                else:
-                    mem_pct = round((mem_res / total_mem) * 100, 1)
+            with open("/proc/uptime") as f:
+                system_uptime_sec = int(float(f.read().split()[0]))
 
-                idx = pid
-                if stats[1] == "(go2rtc)":
-                    idx = "go2rtc"
-                if stats[1].startswith("(frigate.r"):
-                    idx = "recording"
+            clk_tck = os.sysconf(os.sysconf_names["SC_CLK_TCK"])
 
-                usages[idx] = {
-                    "cpu": str(round(cpu_usage, 2)),
-                    "mem": f"{mem_pct}",
-                }
-            except:
-                continue
+            process_utime_sec = utime // clk_tck
+            process_stime_sec = stime // clk_tck
+            process_starttime_sec = starttime // clk_tck
+
+            process_elapsed_sec = system_uptime_sec - process_starttime_sec
+            process_usage_sec = process_utime_sec + process_stime_sec
+            cpu_average_usage = process_usage_sec * 100 // process_elapsed_sec
+
+            with open(f"/proc/{pid}/statm", "r") as f:
+                mem_stats = f.readline().split()
+            mem_res = int(mem_stats[1]) * os.sysconf("SC_PAGE_SIZE") / 1024
+
+            if docker_memlimit > 0:
+                mem_pct = round((mem_res / docker_memlimit) * 100, 1)
+            else:
+                mem_pct = round((mem_res / total_mem) * 100, 1)
+
+            usages[pid] = {
+                "cpu": str(cpu_percent),
+                "cpu_average": str(round(cpu_average_usage, 2)),
+                "mem": f"{mem_pct}",
+            }
+        except:
+            continue
 
     return usages
 
