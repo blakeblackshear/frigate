5358a104a546d120b2992c88c301566473e6ea59
Sergey Krashevich <svk@svk.su>
2023-05-18 08:58:48 +0300

    black...

--- frigate/util.py
+++ frigate/util.py
@@ -866,7 +866,7 @@ def get_bandwidth_stats() -> dict[str, dict]:
             try:
                 if re.search("(^ffmpeg|\/go2rtc)/([0-9]+)/", stats[0]):
                     process = stats[0].split("/")
-                    usages[process[len(process)-2]] = {
+                    usages[process[len(process) - 2]] = {
                         "bandwidth": round(float(stats[2]), 1),
                     }
             except:
