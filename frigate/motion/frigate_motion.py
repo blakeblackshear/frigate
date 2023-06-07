e344dea4c3059f6023cb95fbd555245ea830ebc8
Blake Blackshear <blakeb@blakeshome.com>
2023-06-07 06:55:09 -0500

    create a simplified motion detector

--- frigate/motion/frigate_motion.py
+++ frigate/motion/frigate_motion.py
@@ -12,9 +12,9 @@ class FrigateMotionDetector(MotionDetector):
         frame_shape,
         config: MotionConfig,
         fps: int,
-        improve_contrast_enabled,
-        motion_threshold,
-        motion_contour_area,
+        improve_contrast,
+        threshold,
+        contour_area,
     ):
         self.config = config
         self.frame_shape = frame_shape
@@ -34,9 +34,9 @@ class FrigateMotionDetector(MotionDetector):
         )
         self.mask = np.where(resized_mask == [0])
         self.save_images = False
-        self.improve_contrast = improve_contrast_enabled
-        self.threshold = motion_threshold
-        self.contour_area = motion_contour_area
+        self.improve_contrast = improve_contrast
+        self.threshold = threshold
+        self.contour_area = contour_area
 
     def detect(self, frame):
         motion_boxes = []
@@ -132,18 +132,10 @@ class FrigateMotionDetector(MotionDetector):
                             (0, 0, 255),
                             2,
                         )
-                # print("--------")
-                image_row_1 = cv2.hconcat(
-                    [
-                        cv2.cvtColor(frameDelta, cv2.COLOR_GRAY2BGR),
-                        cv2.cvtColor(avg_delta_image, cv2.COLOR_GRAY2BGR),
-                    ]
+
+                cv2.imwrite(
+                    f"debug/frames/frigate-{self.frame_counter}.jpg", thresh_dilated
                 )
-                image_row_2 = cv2.hconcat(
-                    [cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR), thresh_dilated]
-                )
-                combined_image = cv2.vconcat([image_row_1, image_row_2])
-                cv2.imwrite(f"motion/motion-{self.frame_counter}.jpg", combined_image)
 
         if len(motion_boxes) > 0:
             self.motion_frame_count += 1
