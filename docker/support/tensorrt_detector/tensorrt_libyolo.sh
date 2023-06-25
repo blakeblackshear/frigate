3d7330a6a0b27ad235df58a2c9d6ba463c78bbec
Nate Meyer <nate.devel@gmail.com>
2023-06-24 21:45:48 -0400

    Fix issues in model generation script

--- docker/support/tensorrt_detector/tensorrt_libyolo.sh
+++ docker/support/tensorrt_detector/tensorrt_libyolo.sh
@@ -2,15 +2,17 @@
 
 set -euxo pipefail
 
+SCRIPT_DIR="/usr/local/src/tensorrt_demos"
+
 # Clone tensorrt_demos repo
 git clone --depth 1 https://github.com/NateMeyer/tensorrt_demos.git -b conditional_download
 
 # Build libyolo
 cd ./tensorrt_demos/plugins && make all
 cp libyolo_layer.so /usr/local/lib/libyolo_layer.so
-cp libyolo_layer.so ../yolo/libyolo_layer.so
 
 # Store yolo scripts for later conversion
 cd ../
-mkdir -p /usr/local/src/tensorrt_demos
-cp -a yolo /usr/local/src/tensorrt_demos/
+mkdir -p ${SCRIPT_DIR}/plugins
+cp plugins/libyolo_layer.so ${SCRIPT_DIR}/plugins/libyolo_layer.so
+cp -a yolo ${SCRIPT_DIR}/
