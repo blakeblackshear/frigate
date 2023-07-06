f30ba254445221d1f9fb016457774c11e704048a
Andrew Reiter <andrew.reiter@shield.ai>
2023-07-06 08:31:17 -0400

    Reduce framerate before downscaling (#7022)
    * Reduce framerate before downscaling
    
    It is cheaper to drop frames and downscale those that remain than it is
    to downscale all frames and then drop some of them. This is achieved
    with the filter chain `-cv fps=FPS,scale=W:H`, and perhaps was the
    original intention. The plain `-r` and `-s` flags do not execute in
    order though - they each put themselves at the *end* of the filterchain,
    so `-r FPS -s WxH` actually applies the scale filter first, and then the
    rate filter.
    
    This fix can halve the CPU used by the detect ffmpeg process.
    
    * Bring back hard rate limits

--- frigate/ffmpeg_presets.py
+++ frigate/ffmpeg_presets.py
@@ -107,14 +107,14 @@ PRESETS_HW_ACCEL_DECODE = {
 }
 
 PRESETS_HW_ACCEL_SCALE = {
-    "preset-rpi-32-h264": "-r {0} -s {1}x{2}",
-    "preset-rpi-64-h264": "-r {0} -s {1}x{2}",
+    "preset-rpi-32-h264": "-r {0} -vf fps={0},scale={1}:{2}",
+    "preset-rpi-64-h264": "-r {0} -vf fps={0},scale={1}:{2}",
     "preset-vaapi": "-r {0} -vf fps={0},scale_vaapi=w={1}:h={2},hwdownload,format=yuv420p",
     "preset-intel-qsv-h264": "-r {0} -vf vpp_qsv=framerate={0}:w={1}:h={2}:format=nv12,hwdownload,format=nv12,format=yuv420p",
     "preset-intel-qsv-h265": "-r {0} -vf vpp_qsv=framerate={0}:w={1}:h={2}:format=nv12,hwdownload,format=nv12,format=yuv420p",
     "preset-nvidia-h264": "-r {0} -vf fps={0},scale_cuda=w={1}:h={2}:format=nv12,hwdownload,format=nv12,format=yuv420p",
     "preset-nvidia-h265": "-r {0} -vf fps={0},scale_cuda=w={1}:h={2}:format=nv12,hwdownload,format=nv12,format=yuv420p",
-    "default": "-r {0} -s {1}x{2}",
+    "default": "-r {0} -vf fps={0},scale={1}:{2}",
 }
 
 PRESETS_HW_ACCEL_ENCODE_BIRDSEYE = {
