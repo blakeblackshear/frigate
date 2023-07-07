c4e552354063ebe3d80b6091089f91613c701c85
Nicolas Mowen <nickmowen213@gmail.com>
2023-07-07 06:16:23 -0600

    Hide birdseye width button on mobile devices

--- web/src/routes/Birdseye.jsx
+++ web/src/routes/Birdseye.jsx
@@ -81,7 +81,7 @@ export default function Birdseye() {
         </Heading>
 
         <button
-          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded sm:hidden"
           onClick={() => setIsMaxWidth(!isMaxWidth)}
         >
           Toggle width
