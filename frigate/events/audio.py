b2981b578854f68245962c68491e3ae8250550dc
Nick Mowen <nickmowen213@gmail.com>
2023-07-07 13:31:15 -0600

    Fix event parsing

--- frigate/events/audio.py
+++ frigate/events/audio.py
@@ -196,7 +196,7 @@ class AudioEventMaintainer(threading.Thread):
             )
 
             if resp.status_code == 200:
-                event_id = resp.json()[0]["event_id"]
+                event_id = resp.json()["event_id"]
                 self.detections[label] = {
                     "id": event_id,
                     "label": label,
