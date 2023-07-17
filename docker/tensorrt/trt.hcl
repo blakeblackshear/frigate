b3ce493a16f588e8946a06d9d77dc0aa3a8d9ce6
Nick Mowen <nickmowen213@gmail.com>
2023-07-17 15:19:13 -0600

    Add wheels target

--- docker/tensorrt/trt.hcl
+++ docker/tensorrt/trt.hcl
@@ -1,4 +1,4 @@
-target "deps" {
+target deps {
   dockerfile = "docker/main/Dockerfile"
   platforms = ["linux/amd64"]
 }
@@ -8,11 +8,18 @@ target rootfs {
   platforms = ["linux/amd64"]
 }
 
+target wheels {
+  dockerfile = "docker/main/Dockerfile"
+  platforms = ["linux/amd64"]
+}
+
 target "tensorrt" {
   dockerfile = "docker/tensorrt/Dockerfile"
+  context = "."
   contexts = {
     deps = "target:deps",
     rootfs = "target:rootfs"
+    wheels = "target:wheels"
   }
   platforms = ["linux/amd64"]
 }
--- docker/tensorrt/trt.mk
+++ docker/tensorrt/trt.mk
@@ -1,10 +1,10 @@
 BOARDS += trt
 
 local-trt: version
-	docker buildx bake --file=docker/rpi/bake.hcl --set rpi.tagsfrigate:latest-tensorrt tensorrt
+	docker buildx bake --file=docker/tensorrt/trt.hcl --set tensorrt.tags=frigate:latest-tensorrt tensorrt
 
 build-trt:
-	docker buildx bake --file=docker/rpi/bake.hcl --set rpi.tags=$(IMAGE_REPO):${GITHUB_REF_NAME}-$(COMMIT_HASH)-tensorrt tensorrt
+	docker buildx bake --file=docker/tensorrt/trt.hcl --set tensorrt.tags=$(IMAGE_REPO):${GITHUB_REF_NAME}-$(COMMIT_HASH)-tensorrt tensorrt
 
 push-trt: build-trt
-	docker buildx bake --push --file=docker/rpi/bake.hcl --set rpi.tags=$(IMAGE_REPO):${GITHUB_REF_NAME}-$(COMMIT_HASH)-tensorrt tensorrt
+	docker buildx bake --push --file=docker/tensorrt/trt.hcl --set tensorrt.tags=$(IMAGE_REPO):${GITHUB_REF_NAME}-$(COMMIT_HASH)-tensorrt tensorrt
