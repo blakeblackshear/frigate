0add91af9acc93d1a4fd707427255b69131e9fdb
Nick Mowen <nickmowen213@gmail.com>
2023-07-18 08:10:52 -0600

    Add back --load

--- docker/tensorrt/trt.mk
+++ docker/tensorrt/trt.mk
@@ -1,7 +1,7 @@
 BOARDS += trt
 
 local-trt: version
-	docker buildx bake --file=docker/tensorrt/trt.hcl --set tensorrt.tags=frigate:latest-tensorrt tensorrt
+	docker buildx bake --load --file=docker/tensorrt/trt.hcl --set tensorrt.tags=frigate:latest-tensorrt tensorrt
 
 build-trt:
 	docker buildx bake --file=docker/tensorrt/trt.hcl --set tensorrt.tags=$(IMAGE_REPO):${GITHUB_REF_NAME}-$(COMMIT_HASH)-tensorrt tensorrt
