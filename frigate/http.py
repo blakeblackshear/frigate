2b9965630979030224e156a84faedff0533e7f17
Nick Mowen <nickmowen213@gmail.com>
2023-07-28 06:51:05 -0600

    Formatting

--- frigate/http.py
+++ frigate/http.py
@@ -1607,7 +1607,7 @@ def vod_event(id):
     "/export/<camera_name>/start/<int:start_time>/end/<int:end_time>", methods=["POST"]
 )
 @bp.route(
-    "/export/<camera_name>/start/<float:start_time>/end/<float:end_time>", 
+    "/export/<camera_name>/start/<float:start_time>/end/<float:end_time>",
     methods=["POST"],
 )
 def export_recording(camera_name: str, start_time, end_time):
