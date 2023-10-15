8b035ff21d8cad967f4c796cd4a63f76ab0a81a1
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2023-10-14 19:05:19 -0500

    non-working tests

--- web/src/api/__tests__/ws.test.jsx
+++ web/src/api/__tests__/ws.test.jsx
@@ -1,10 +1,11 @@
 import { h } from 'preact';
-import { WS, WsProvider, useWs } from '../ws';
+import { WS as frigateWS, WsProvider, useWs } from '../ws';
 import { useCallback, useContext } from 'preact/hooks';
 import { fireEvent, render, screen } from 'testing-library';
+import { WS } from "jest-websocket-mock";
 
 function Test() {
-  const { state } = useContext(WS);
+  const { state } = useContext(frigateWS);
   return state.__connected ? (
     <div data-testid="data">
       {Object.keys(state).map((key) => (
@@ -19,38 +20,26 @@ function Test() {
 const TEST_URL = 'ws://test-foo:1234/ws';
 
 describe('WsProvider', () => {
-  let createWebsocket, wsClient;
-  beforeEach(() => {
+  let wsClient, wsServer;
+  beforeEach( async () => {
     wsClient = {
       close: vi.fn(),
       send: vi.fn(),
     };
-    createWebsocket = vi.fn((url) => {
-      wsClient.args = [url];
-      return new Proxy(
-        {},
-        {
-          get(_target, prop, _receiver) {
-            return wsClient[prop];
-          },
-          set(_target, prop, value) {
-            wsClient[prop] = typeof value === 'function' ? vi.fn(value) : value;
-            if (prop === 'onopen') {
-              wsClient[prop]();
-            }
-            return true;
-          },
-        }
-      );
-    });
+    wsServer = new WS(TEST_URL);
+  });
+
+  afterEach(() => {
+    WS.clean();
   });
 
   test('connects to the ws server', async () => {
     render(
-      <WsProvider config={mockConfig} createWebsocket={createWebsocket} wsUrl={TEST_URL}>
+      <WsProvider config={mockConfig} wsUrl={TEST_URL}>
         <Test />
       </WsProvider>
     );
+    await wsServer.connected;
     await screen.findByTestId('data');
     expect(wsClient.args).toEqual([TEST_URL]);
     expect(screen.getByTestId('__connected')).toHaveTextContent('true');
@@ -71,16 +60,17 @@ describe('WsProvider', () => {
     }
 
     const { rerender } = render(
-      <WsProvider config={mockConfig} createWebsocket={createWebsocket} wsUrl={TEST_URL}>
+      <WsProvider config={mockConfig} wsUrl={TEST_URL}>
         <Test />
       </WsProvider>
     );
+    await wsServer.connected;
     await screen.findByTestId('payload');
     wsClient.onmessage({
       data: JSON.stringify({ topic: 'tacos', payload: JSON.stringify({ yes: true }), retain: false }),
     });
     rerender(
-      <WsProvider config={mockConfig} createWebsocket={createWebsocket} wsUrl={TEST_URL}>
+      <WsProvider config={mockConfig} wsUrl={TEST_URL}>
         <Test />
       </WsProvider>
     );
@@ -98,10 +88,11 @@ describe('WsProvider', () => {
     }
 
     render(
-      <WsProvider config={mockConfig} createWebsocket={createWebsocket} wsUrl={TEST_URL}>
+      <WsProvider config={mockConfig} wsUrl={TEST_URL}>
         <Test />
       </WsProvider>
     );
+    await wsServer.connected;
     await screen.findByRole('button');
     fireEvent.click(screen.getByRole('button'));
     await expect(wsClient.send).toHaveBeenCalledWith(
@@ -118,10 +109,11 @@ describe('WsProvider', () => {
       },
     };
     render(
-      <WsProvider config={config} createWebsocket={createWebsocket} wsUrl={TEST_URL}>
+      <WsProvider config={config} wsUrl={TEST_URL}>
         <Test />
       </WsProvider>
     );
+    await wsServer.connected;
     await screen.findByTestId('data');
     expect(screen.getByTestId('front/detect/state')).toHaveTextContent(
       '{"lastUpdate":123456,"payload":"ON","retain":false}'
