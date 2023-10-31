cf19de15bc19810ddebfdba2119510ccc94d0642
tpjanssen <25168870+tpjanssen@users.noreply.github.com>
2023-10-31 16:45:06 +0100

    Revert "Update http.py"
    This reverts commit fa292682d6fb157941418ee6027a0327b078b828.

--- frigate/http.py
+++ frigate/http.py
@@ -1388,7 +1388,6 @@ def get_snapshot_from_recording(camera_name: str, frame_time: str):
             )
         )
         .where(Recordings.camera == camera_name)
-        .order_by(Recordings.start_time.desc())
     )
 
     try:
