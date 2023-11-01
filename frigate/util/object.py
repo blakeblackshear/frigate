bf072e7241bb4f375b7a387cf73f9f85bd984374
Nicolas Mowen <nickmowen213@gmail.com>
2023-11-01 13:09:06 -0600

    Fix region when no data in grid

--- frigate/util/object.py
+++ frigate/util/object.py
@@ -176,7 +176,7 @@ def get_region_from_grid(
 
     # if there is no known data, get standard region for motion box
     if not cell or not cell["sizes"]:
-        return calculate_region(frame_shape, box[0], box[1], box[2], box[3], min_region)
+        return box
 
     # convert the calculated region size to relative
     calc_size = (box[2] - box[0]) / frame_shape[1]
