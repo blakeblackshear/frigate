fd6a3bd5d226535c25bccb4f8f4fd0f0907fe25b
tpjanssen <25168870+tpjanssen@users.noreply.github.com>
2023-11-01 12:14:51 +0100

    API recordings snapsnot PNG fix (#8401)
    * Update http.py
    
    * Update http.py
    
    Limit query results

--- frigate/http.py
+++ frigate/http.py
@@ -1388,6 +1388,8 @@ def get_snapshot_from_recording(camera_name: str, frame_time: str):
             )
         )
         .where(Recordings.camera == camera_name)
+        .order_by(Recordings.start_time.desc())
+        .limit(1)
     )
 
     try:
