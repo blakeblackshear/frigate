dbd628f0482e07e0caf44c68326bce89b70eeb15
Nicolas Mowen <nickmowen213@gmail.com>
2023-12-12 16:21:42 -0700

    Add export page in to new web UI (#8929)

---
+++ web-new/src/components/card/ExportCard.tsx
@@ -0,0 +1,50 @@
+import { baseUrl } from "@/api/baseUrl";
+import ActivityIndicator from "../ui/activity-indicator";
+import { Card } from "../ui/card";
+import { LuPlay, LuTrash } from "react-icons/lu";
+import { Button } from "../ui/button";
+
+type ExportProps = {
+  file: {
+    name: string;
+  };
+  onSelect: (file: string) => void;
+  onDelete: (file: string) => void;
+};
+
+export default function ExportCard({ file, onSelect, onDelete }: ExportProps) {
+  return (
+    <Card className="my-4 p-4 bg-secondary flex justify-start text-center items-center">
+      {file.name.startsWith("in_progress") ? (
+        <>
+          <div className="p-2">
+            <ActivityIndicator size={16} />
+          </div>
+          <div className="px-2">
+            {file.name.substring(12, file.name.length - 4)}
+          </div>
+        </>
+      ) : (
+        <>
+          <Button variant="secondary" onClick={() => onSelect(file.name)}>
+            <LuPlay className="h-4 w-4 text-green-600" />
+          </Button>
+          <a
+            className="text-blue-500 hover:underline overflow-hidden"
+            href={`${baseUrl}exports/${file.name}`}
+            download
+          >
+            {file.name.substring(0, file.name.length - 4)}
+          </a>
+          <Button
+            className="ml-auto"
+            variant="secondary"
+            onClick={() => onDelete(file.name)}
+          >
+            <LuTrash className="h-4 w-4" stroke="#f87171" />
+          </Button>
+        </>
+      )}
+    </Card>
+  );
+}
---
+++ web-new/src/components/player/VideoPlayer.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useRef, ReactElement } from "react";
+import videojs from 'video.js';
+import 'videojs-playlist';
+import 'video.js/dist/video-js.css';
+import Player from "video.js/dist/types/player";
+
+type VideoPlayerProps = {
+    children?: ReactElement | ReactElement[],
+    options?: {
+        [key: string]: any
+    },
+    seekOptions?: {
+        forward?: number,
+        backward?: number,
+    },
+    onReady?: (player: Player) => void,
+    onDispose?: () => void,
+}
+
+export default function VideoPlayer({ children, options, seekOptions = {forward:30, backward: 10}, onReady = (_) => {}, onDispose = () => {} }: VideoPlayerProps) {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+    const playerRef = useRef<Player | null>(null);
+
+    useEffect(() => {
+      const defaultOptions = {
+        controls: true,
+        controlBar: {
+          skipButtons: seekOptions,
+        },
+        playbackRates: [0.5, 1, 2, 4, 8],
+        fluid: true,
+      };
+
+
+      if (!videojs.browser.IS_FIREFOX) {
+        defaultOptions.playbackRates.push(16);
+      }
+
+      // Make sure Video.js player is only initialized once
+      if (!playerRef.current) {
+        // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
+        const videoElement = document.createElement("video-js");
+
+        videoElement.classList.add('small-player');
+        videoElement.classList.add('video-js');
+        videoElement.classList.add('vjs-default-skin');
+        videoRef.current.appendChild(videoElement);
+
+        const player = playerRef.current = videojs(videoElement, { ...defaultOptions, ...options }, () => {
+          onReady && onReady(player);
+        });
+      }
+    }, [options, videoRef]);
+
+    // Dispose the Video.js player when the functional component unmounts
+    useEffect(() => {
+      const player = playerRef.current;
+
+      return () => {
+        if (player && !player.isDisposed()) {
+          player.dispose();
+          playerRef.current = null;
+          onDispose();
+        }
+      };
+    }, [playerRef]);
+
+    return (
+      <div data-vjs-player>
+        <div ref={videoRef} />
+        {children}
+      </div>
+    );
+  }
