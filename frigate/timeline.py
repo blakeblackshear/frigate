3aaa8ae338742d0f6a82e895eac1174640a11f58
Nicolas Mowen <nickmowen213@gmail.com>
2023-12-12 19:48:52 -0700

    Add initial implementation of history view in new webui framework (#8895)
    * Add support for review grid
    
    * Cleanup reloading on focus
    
    * Adjust timeline api to include metadata and before
    
    * Be more efficient about getting info
    
    * Adjust to new data format
    
    * Cleanup types
    
    * Cleanup text
    
    * Transition to history
    
    * Cleanup
    
    * remove old web implementations
    
    * Cleanup

--- frigate/timeline.py
+++ frigate/timeline.py
@@ -29,6 +29,7 @@ class TimelineProcessor(threading.Thread):
         self.config = config
         self.queue = queue
         self.stop_event = stop_event
+        self.pre_event_cache: dict[str, list[dict[str, any]]] = {}
 
     def run(self) -> None:
         while not self.stop_event.is_set():
@@ -48,14 +49,39 @@ class TimelineProcessor(threading.Thread):
                     camera, event_type, prev_event_data, event_data
                 )
 
+    def insert_or_save(
+        self,
+        entry: dict[str, any],
+        prev_event_data: dict[any, any],
+        event_data: dict[any, any],
+    ) -> None:
+        """Insert into db or cache."""
+        id = entry[Timeline.source_id]
+        if not event_data["has_clip"] and not event_data["has_snapshot"]:
+            # the related event has not been saved yet, should be added to cache
+            if id in self.pre_event_cache.keys():
+                self.pre_event_cache[id].append(entry)
+            else:
+                self.pre_event_cache[id] = [entry]
+        else:
+            # the event is saved, insert to db and insert cached into db
+            if id in self.pre_event_cache.keys():
+                for e in self.pre_event_cache[id]:
+                    Timeline.insert(e).execute()
+
+                self.pre_event_cache.pop(id)
+
+            Timeline.insert(entry).execute()
+
     def handle_object_detection(
         self,
         camera: str,
         event_type: str,
         prev_event_data: dict[any, any],
         event_data: dict[any, any],
-    ) -> None:
+    ) -> bool:
         """Handle object detection."""
+        save = False
         camera_config = self.config.cameras[camera]
 
         timeline_entry = {
@@ -70,6 +96,7 @@ class TimelineProcessor(threading.Thread):
                     event_data["box"],
                 ),
                 "label": event_data["label"],
+                "sub_label": event_data.get("sub_label"),
                 "region": to_relative_box(
                     camera_config.detect.width,
                     camera_config.detect.height,
@@ -80,41 +107,36 @@ class TimelineProcessor(threading.Thread):
         }
         if event_type == "start":
             timeline_entry[Timeline.class_type] = "visible"
-            Timeline.insert(timeline_entry).execute()
+            save = True
         elif event_type == "update":
-            # zones have been updated
             if (
-                prev_event_data["current_zones"] != event_data["current_zones"]
-                and len(event_data["current_zones"]) > 0
+                len(prev_event_data["current_zones"]) < len(event_data["current_zones"])
                 and not event_data["stationary"]
             ):
                 timeline_entry[Timeline.class_type] = "entered_zone"
                 timeline_entry[Timeline.data]["zones"] = event_data["current_zones"]
-                Timeline.insert(timeline_entry).execute()
+                save = True
             elif prev_event_data["stationary"] != event_data["stationary"]:
                 timeline_entry[Timeline.class_type] = (
                     "stationary" if event_data["stationary"] else "active"
                 )
-                Timeline.insert(timeline_entry).execute()
+                save = True
             elif prev_event_data["attributes"] == {} and event_data["attributes"] != {}:
                 timeline_entry[Timeline.class_type] = "attribute"
                 timeline_entry[Timeline.data]["attribute"] = list(
                     event_data["attributes"].keys()
                 )[0]
-                Timeline.insert(timeline_entry).execute()
+                save = True
             elif not prev_event_data.get("sub_label") and event_data.get("sub_label"):
                 sub_label = event_data["sub_label"][0]
 
                 if sub_label not in ALL_ATTRIBUTE_LABELS:
                     timeline_entry[Timeline.class_type] = "sub_label"
                     timeline_entry[Timeline.data]["sub_label"] = sub_label
-                    Timeline.insert(timeline_entry).execute()
+                    save = True
         elif event_type == "end":
-            if event_data["has_clip"] or event_data["has_snapshot"]:
-                timeline_entry[Timeline.class_type] = "gone"
-                Timeline.insert(timeline_entry).execute()
-            else:
-                # if event was not saved then the timeline entries should be deleted
-                Timeline.delete().where(
-                    Timeline.source_id == event_data["id"]
-                ).execute()
+            timeline_entry[Timeline.class_type] = "gone"
+            save = True
+
+        if save:
+            self.insert_or_save(timeline_entry, prev_event_data, event_data)
