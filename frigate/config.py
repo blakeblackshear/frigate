7dc09b0c79d409458cbece70db4f3a5ede7fee3d
Nick Mowen <nickmowen213@gmail.com>
2023-12-31 05:26:30 -0700

    Automatically detect nvidia / vaapi hwaccel args

--- frigate/config.py
+++ frigate/config.py
@@ -39,7 +39,7 @@ from frigate.util.builtin import (
     load_config_with_no_duplicates,
 )
 from frigate.util.image import create_mask
-from frigate.util.services import get_video_properties
+from frigate.util.services import auto_detect_hwaccel, get_video_properties
 
 logger = logging.getLogger(__name__)
 
@@ -599,8 +599,8 @@ class FfmpegConfig(FrigateBaseModel):
     global_args: Union[str, List[str]] = Field(
         default=FFMPEG_GLOBAL_ARGS_DEFAULT, title="Global FFmpeg arguments."
     )
-    hwaccel_args: Union[str, List[str]] = Field(
-        default_factory=list, title="FFmpeg hardware acceleration arguments."
+    hwaccel_args: Optional[Union[str, List[str]]] = Field(
+        default=None, title="FFmpeg hardware acceleration arguments."
     )
     input_args: Union[str, List[str]] = Field(
         default=FFMPEG_INPUT_ARGS_DEFAULT, title="FFmpeg input arguments."
@@ -1096,6 +1096,10 @@ class FrigateConfig(FrigateBaseModel):
                 config.objects.filters[attribute] = FilterConfig(min_score=0.7)
             elif config.objects.filters[attribute].min_score == 0.5:
                 config.objects.filters[attribute].min_score = 0.7
+
+        # auto detect hwaccel args
+        if config.ffmpeg.hwaccel_args is None:
+            config.ffmpeg.hwaccel_args = auto_detect_hwaccel()
 
         # Global config to propagate down to camera level
         global_config = config.dict(
