d35a2f17953e12bdc343e3118fd8beb3b3bba11f
Indrek Mandre <indrek@mare.ee>
2024-02-04 16:56:36 +0200

    docker/rocm: also build a version for gfx1100

--- docker/rocm/rocm.mk
+++ docker/rocm/rocm.mk
@@ -1,7 +1,7 @@
 BOARDS += rocm
 
 # AMD/ROCm is chunky so we build couple of smaller images for specific chipsets
-ROCM_CHIPSETS:=gfx900:9.0.0 gfx1030:10.3.0
+ROCM_CHIPSETS:=gfx900:9.0.0 gfx1030:10.3.0 gfx1100:11.0.0
 
 local-rocm: version
 	$(foreach chipset,$(ROCM_CHIPSETS),AMDGPU=$(word 1,$(subst :, ,$(chipset))) HSA_OVERRIDE_GFX_VERSION=$(word 2,$(subst :, ,$(chipset))) HSA_OVERRIDE=1 docker buildx bake --load --file=docker/rocm/rocm.hcl --set rocm.tags=frigate:latest-rocm-$(word 1,$(subst :, ,$(chipset))) rocm;)
