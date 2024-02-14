5269b0810c41681e02de31d2ed617a2c2dede313
Nicolas Mowen <nickmowen213@gmail.com>
2024-02-14 09:42:19 -0700

    Use correct aspect ratio on all devices

--- web/src/components/image/AnimatedEventThumbnail.tsx
+++ web/src/components/image/AnimatedEventThumbnail.tsx
@@ -4,12 +4,15 @@ import TimeAgo from "../dynamic/TimeAgo";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 import { useMemo } from "react";
 import { useApiHost } from "@/api";
+import useSWR from "swr";
+import { FrigateConfig } from "@/types/frigateConfig";
 
 type AnimatedEventThumbnailProps = {
   event: FrigateEvent;
 };
 export function AnimatedEventThumbnail({ event }: AnimatedEventThumbnailProps) {
   const apiHost = useApiHost();
+  const { data: config } = useSWR<FrigateConfig>("config");
 
   const imageUrl = useMemo(() => {
     if (Date.now() / 1000 < event.start_time + 20) {
@@ -19,11 +22,25 @@ export function AnimatedEventThumbnail({ event }: AnimatedEventThumbnailProps) {
     return `${baseUrl}api/events/${event.id}/preview.gif`;
   }, [event]);
 
+  const aspectRatio = useMemo(() => {
+    if (!config) {
+      return 1;
+    }
+
+    const detect = config.cameras[event.camera].detect;
+    return detect.width / detect.height;
+  }, [config]);
+
   return (
     <Tooltip>
       <TooltipTrigger asChild>
-        <div className="relative h-24 mr-4">
-          <img className="h-full w-min rounded" src={imageUrl} />
+        <div
+          className="h-24 relative rounded bg-cover bg-no-repeat bg-center mr-4"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            aspectRatio: aspectRatio,
+          }}
+        >
           <div className="absolute bottom-0 w-full h-6 bg-gradient-to-t from-slate-900/50 to-transparent rounded">
             <div className="absolute left-1 bottom-0 text-xs text-white w-full">
               <TimeAgo time={event.start_time * 1000} dense />
