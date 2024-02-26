2699d5ddc2dc3af4a32e841aa09282e9fe8ce557
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2024-02-26 07:48:34 -0600

    fix gradients and lazy load with skeleton

--- web/src/components/player/PreviewThumbnailPlayer.tsx
+++ web/src/components/player/PreviewThumbnailPlayer.tsx
@@ -7,7 +7,7 @@ import { getIconForLabel, getIconForSubLabel } from "@/utils/iconUtil";
 import TimeAgo from "../dynamic/TimeAgo";
 import useSWR from "swr";
 import { FrigateConfig } from "@/types/frigateConfig";
-import { isMobile, isSafari } from "react-device-detect";
+import { isFirefox, isMobile, isSafari } from "react-device-detect";
 import Chip from "../Chip";
 import {
   ContextMenu,
@@ -20,6 +20,7 @@ import { LuCheckSquare, LuFileUp, LuTrash } from "react-icons/lu";
 import axios from "axios";
 import { useFormattedTimestamp } from "@/hooks/use-date-utils";
 import { Skeleton } from "../ui/skeleton";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 type PreviewPlayerProps = {
   review: ReviewSegment;
@@ -121,12 +122,10 @@ export default function PreviewThumbnailPlayer({
               />
             </div>
           )}
-          {!imgLoaded && <Skeleton className="w-full h-full rounded-2xl" />}
-          <img
+          <LazyLoadImage
             className={`w-full h-full transition-opacity ${
-              playingBack ? "opacity-0" : "opacity-100"
+              playingBack && imgLoaded ? "opacity-0" : "opacity-100"
             }`}
-            loading="lazy"
             src={`${apiHost}${review.thumb_path.replace(
               "/media/frigate/",
               ""
@@ -134,30 +133,37 @@ export default function PreviewThumbnailPlayer({
             onLoad={() => {
               setImgLoaded(true);
             }}
+            placeholder={<Skeleton className="w-full h-full rounded-xl" />}
           />
 
-          {imgLoaded &&
-            (review.severity == "alert" || review.severity == "detection") && (
-              <Chip className="absolute top-2 left-2 flex gap-1 bg-gradient-to-br from-gray-400 to-gray-500 bg-gray-500 z-0">
-                {review.data.objects.map((object) => {
-                  return getIconForLabel(object, "w-3 h-3 text-white");
-                })}
-                {review.data.audio.map((audio) => {
-                  return getIconForLabel(audio, "w-3 h-3 text-white");
-                })}
-                {review.data.sub_labels?.map((sub) => {
-                  return getIconForSubLabel(sub, "w-3 h-3 text-white");
-                })}
-              </Chip>
-            )}
           {!playingBack && imgLoaded && (
-            <div className="absolute left-[6px] right-[6px] bottom-1 flex justify-between text-white">
-              <TimeAgo time={review.start_time * 1000} dense />
-              {formattedDate}
-            </div>
+            <>
+              <div className="absolute top-0 left-0 right-0 rounded-t-l z-10 w-full h-[30%] bg-gradient-to-b from-black/60 to-transparent pointer-events-none">
+                <div className="flex h-full justify-between items-start mx-3 pb-1 text-white text-sm ">
+                  {(review.severity == "alert" ||
+                    review.severity == "detection") && (
+                    <Chip className="absolute top-2 left-2 flex gap-1 bg-gradient-to-br from-gray-400 to-gray-500 bg-gray-500 z-0">
+                      {review.data.objects.map((object) => {
+                        return getIconForLabel(object, "w-3 h-3 text-white");
+                      })}
+                      {review.data.audio.map((audio) => {
+                        return getIconForLabel(audio, "w-3 h-3 text-white");
+                      })}
+                      {review.data.sub_labels?.map((sub) => {
+                        return getIconForSubLabel(sub, "w-3 h-3 text-white");
+                      })}
+                    </Chip>
+                  )}
+                </div>
+              </div>
+              <div className="absolute bottom-0 left-0 right-0 rounded-b-l z-10 w-full h-[20%] bg-gradient-to-t from-black/60 to-transparent pointer-events-none">
+                <div className="flex h-full justify-between items-end mx-3 pb-1 text-white text-sm ">
+                  <TimeAgo time={review.start_time * 1000} dense />
+                  {formattedDate}
+                </div>
+              </div>
+            </>
           )}
-          <div className="absolute top-0 left-0 right-0 rounded-2xl z-10 w-full h-[30%] bg-gradient-to-b from-black/20 to-transparent pointer-events-none" />
-          <div className="absolute bottom-0 left-0 right-0 rounded-2xl z-10 w-full h-[10%] bg-gradient-to-t from-black/20 to-transparent pointer-events-none" />
           {playingBack && (
             <Slider
               className="absolute left-0 right-0 bottom-0 z-10"
@@ -168,7 +174,7 @@ export default function PreviewThumbnailPlayer({
             />
           )}
           {!playingBack && imgLoaded && review.has_been_reviewed && (
-            <div className="absolute left-0 top-0 bottom-0 right-0 bg-black bg-opacity-60" />
+            <div className="absolute left-0 top-0 bottom-0 right-0 z-10 bg-black bg-opacity-60" />
           )}
         </div>
       </ContextMenuTrigger>
@@ -214,7 +220,7 @@ function PreviewContent({
       return;
     }
 
-    if (isSafari) {
+    if (isSafari || (isFirefox && isMobile)) {
       playerRef.current.pause();
       setManualPlayback(true);
     } else {
@@ -352,9 +358,8 @@ function InProgressPreview({
 
   if (!previewFrames || previewFrames.length == 0) {
     return (
-      <img
+      <LazyLoadImage
         className="h-full w-full"
-        loading="lazy"
         src={`${apiHost}${review.thumb_path.replace("/media/frigate/", "")}`}
       />
     );
