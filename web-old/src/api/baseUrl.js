f8d4a52154969bd8190f71971ff6ab8e1a83efe7
Nicolas Mowen <nickmowen213@gmail.com>
2024-02-26 07:05:40 -0700

    Delete web-old directory

--- web-old/src/api/baseUrl.js
+++
@@ -1,1 +0,0 @@
-export const baseUrl = `${window.location.protocol}//${window.location.host}${window.baseUrl || '/'}`;
--- web-old/src/components/Heading.jsx
+++
@@ -1,5 +0,0 @@
-import { h } from 'preact';
-
-export default function Heading({ children, className = '', size = '2xl' }) {
-  return <h1 className={`font-semibold tracking-widest uppercase text-${size} ${className}`}>{children}</h1>;
-}
