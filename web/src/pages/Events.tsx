aa1224442591156a0b5a90dc7beb7ef419cdc445
Nicolas Mowen <nickmowen213@gmail.com>
2024-02-26 13:47:16 -0700

    Move websocket to top level events

--- web/src/pages/Events.tsx
+++ web/src/pages/Events.tsx
@@ -1,3 +1,4 @@
+import { useFrigateEvents } from "@/api/ws";
 import useApiFilter from "@/hooks/use-api-filter";
 import useOverlayState from "@/hooks/use-overlay-state";
 import { ReviewFilter, ReviewSegment } from "@/types/review";
@@ -5,7 +6,7 @@ import DesktopEventView from "@/views/events/DesktopEventView";
 import DesktopRecordingView from "@/views/events/DesktopRecordingView";
 import MobileEventView from "@/views/events/MobileEventView";
 import axios from "axios";
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { isMobile } from "react-device-detect";
 import useSWR from "swr";
 import useSWRInfinite from "swr/infinite";
@@ -183,6 +184,25 @@ export default function Events() {
     };
   }, [selectedReviewId, reviewPages]);
 
+  // review updates
+
+  const { payload: eventUpdate } = useFrigateEvents();
+  const [hasUpdate, setHasUpdate] = useState(false);
+  useEffect(() => {
+    if (!eventUpdate) {
+      return;
+    }
+
+    // if event is ended and was saved, update events list
+    if (
+      eventUpdate.type == "end" &&
+      (eventUpdate.after.has_clip || eventUpdate.after.has_snapshot)
+    ) {
+      setHasUpdate(true);
+      return;
+    }
+  }, [eventUpdate]);
+
   if (selectedData) {
     return (
       <DesktopRecordingView
@@ -199,8 +219,11 @@ export default function Events() {
           relevantPreviews={allPreviews}
           reachedEnd={isDone}
           isValidating={isValidating}
+          hasUpdate={hasUpdate}
+          setHasUpdate={setHasUpdate}
           loadNextPage={onLoadNextPage}
           markItemAsReviewed={markItemAsReviewed}
+          pullLatestData={updateSegments}
         />
       );
     }
@@ -213,6 +236,8 @@ export default function Events() {
         reachedEnd={isDone}
         isValidating={isValidating}
         filter={reviewFilter}
+        hasUpdate={hasUpdate}
+        setHasUpdate={setHasUpdate}
         loadNextPage={onLoadNextPage}
         markItemAsReviewed={markItemAsReviewed}
         onSelectReview={setSelectedReviewId}
