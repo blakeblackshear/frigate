e05d4280600d7adb66accb29427bf44d2433b444
Nicolas Mowen <nickmowen213@gmail.com>
2024-02-28 08:41:31 -0700

    Add ability to open to recordings from event gif

--- web/src/components/image/AnimatedEventThumbnail.tsx
+++ web/src/components/image/AnimatedEventThumbnail.tsx
@@ -1,11 +1,12 @@
 import { baseUrl } from "@/api/baseUrl";
 import TimeAgo from "../dynamic/TimeAgo";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { useApiHost } from "@/api";
 import useSWR from "swr";
 import { FrigateConfig } from "@/types/frigateConfig";
 import { ReviewSegment } from "@/types/review";
+import { useNavigate } from "react-router-dom";
 
 type AnimatedEventThumbnailProps = {
   event: ReviewSegment;
@@ -13,6 +14,15 @@ type AnimatedEventThumbnailProps = {
 export function AnimatedEventThumbnail({ event }: AnimatedEventThumbnailProps) {
   const apiHost = useApiHost();
   const { data: config } = useSWR<FrigateConfig>("config");
+
+  // interaction
+
+  const navigate = useNavigate();
+  const onOpenReview = useCallback(() => {
+    navigate("events", { state: { review: event.id } });
+  }, [navigate, event]);
+
+  // image behavior
 
   const imageUrl = useMemo(() => {
     if (Date.now() / 1000 < event.start_time + 20) {
@@ -35,11 +45,12 @@ export function AnimatedEventThumbnail({ event }: AnimatedEventThumbnailProps) {
     <Tooltip>
       <TooltipTrigger asChild>
         <div
-          className="h-24 relative rounded bg-cover bg-no-repeat bg-center mr-4"
+          className="h-24 relative rounded bg-cover bg-no-repeat bg-center mr-4 cursor-pointer"
           style={{
             backgroundImage: `url(${imageUrl})`,
             aspectRatio: aspectRatio,
           }}
+          onClick={onOpenReview}
         >
           <div className="absolute bottom-0 inset-x-0 h-6 bg-gradient-to-t from-slate-900/50 to-transparent rounded">
             <div className="w-full absolute left-1 bottom-0 text-xs text-white">
