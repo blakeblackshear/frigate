55030c1655b78750ce3424b0004cc9500fea8f54
Nicolas Mowen <nickmowen213@gmail.com>
2024-02-28 21:04:44 -0700

    Fix

--- frigate/http.py
+++ frigate/http.py
@@ -1384,7 +1384,7 @@ def end_event(event_id):
 
 @bp.route("/config")
 def config():
-    config = current_app.frigate_config.dict()
+    config = current_app.frigate_config.model_dump()
 
     # remove the mqtt password
     config["mqtt"].pop("password", None)
