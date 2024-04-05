9bb1a415d4f83b27caabfb288224cff993a702ec
Nicolas Mowen <nickmowen213@gmail.com>
2024-04-05 08:18:29 -0600

    Add ability to search exports

--- web/src/pages/Export.tsx
+++ web/src/pages/Export.tsx
@@ -10,8 +10,9 @@ import {
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import axios from "axios";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import useSWR from "swr";
 
 type ExportItem = {
@@ -19,24 +20,30 @@ type ExportItem = {
 };
 
 function Export() {
-  const { data: exports, mutate } = useSWR<ExportItem[]>(
+  const { data: allExports, mutate } = useSWR<ExportItem[]>(
     "exports/",
     (url: string) => axios({ baseURL: baseUrl, url }).then((res) => res.data),
   );
 
-  const [deleteClip, setDeleteClip] = useState<string | undefined>();
+  // Search
 
-  const onHandleRename = useCallback(
-    (original: string, update: string) => {
-      axios.patch(`export/${original}/${update}`).then((response) => {
-        if (response.status == 200) {
-          setDeleteClip(undefined);
-          mutate();
-        }
-      });
-    },
-    [mutate],
-  );
+  const [search, setSearch] = useState("");
+
+  const exports = useMemo(() => {
+    if (!search || !allExports) {
+      return allExports;
+    }
+
+    return allExports.filter((exp) =>
+      exp.name
+        .toLowerCase()
+        .includes(search.toLowerCase().replaceAll(" ", "_")),
+    );
+  }, [allExports, search]);
+
+  // Deleting
+
+  const [deleteClip, setDeleteClip] = useState<string | undefined>();
 
   const onHandleDelete = useCallback(() => {
     if (!deleteClip) {
@@ -51,8 +58,22 @@ function Export() {
     });
   }, [deleteClip, mutate]);
 
+  // Renaming
+
+  const onHandleRename = useCallback(
+    (original: string, update: string) => {
+      axios.patch(`export/${original}/${update}`).then((response) => {
+        if (response.status == 200) {
+          setDeleteClip(undefined);
+          mutate();
+        }
+      });
+    },
+    [mutate],
+  );
+
   return (
-    <div className="size-full p-2 overflow-hidden flex flex-col">
+    <div className="size-full p-2 overflow-hidden flex flex-col gap-2">
       <AlertDialog
         open={deleteClip != undefined}
         onOpenChange={() => setDeleteClip(undefined)}
@@ -73,12 +94,24 @@ function Export() {
         </AlertDialogContent>
       </AlertDialog>
 
+      <div className="w-full p-2 flex items-center justify-center">
+        <Input
+          className="w-full md:w-1/3 bg-muted"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
       <div className="w-full overflow-hidden">
-        {exports && (
+        {allExports && exports && (
           <div className="size-full grid gap-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 overflow-y-auto">
-            {Object.values(exports).map((item) => (
+            {Object.values(allExports).map((item) => (
               <ExportCard
                 key={item.name}
+                className={
+                  search == "" || exports.includes(item) ? "" : "hidden"
+                }
                 file={item}
                 onRename={onHandleRename}
                 onDelete={(file) => setDeleteClip(file)}
