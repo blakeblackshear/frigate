5b8eb9691f2912cd4e7c3b10ea8763e110b44ef4
Nicolas Mowen <nickmowen213@gmail.com>
2024-04-29 06:37:04 -0600

    Make bottom bar items easier to tap

--- web/src/components/menu/AccountSettings.tsx
+++ web/src/components/menu/AccountSettings.tsx
@@ -6,12 +6,15 @@ import {
 import { isDesktop } from "react-device-detect";
 import { VscAccount } from "react-icons/vsc";
 
-export default function AccountSettings() {
+type AccountSettingsProps = {
+  className?: string;
+};
+export default function AccountSettings({ className }: AccountSettingsProps) {
   return (
     <Tooltip>
       <TooltipTrigger asChild>
         <div
-          className={`flex flex-col justify-center items-center ${isDesktop ? "rounded-lg text-secondary-foreground bg-secondary hover:bg-muted cursor-pointer" : "text-secondary-foreground"}`}
+          className={`flex flex-col justify-center items-center ${isDesktop ? "rounded-lg text-secondary-foreground bg-secondary hover:bg-muted cursor-pointer" : "text-secondary-foreground"} ${className ?? ""}`}
         >
           <VscAccount className="size-5 md:m-[6px]" />
         </div>
