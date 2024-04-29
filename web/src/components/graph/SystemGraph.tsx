1c90f62ea51bd9efb8d57434b9b8abbe6eaeaa91
Sean Kelly <xconverge@users.noreply.github.com>
2024-04-28 21:00:57 -0700

    Fix rounding (#11147)

--- web/src/components/graph/SystemGraph.tsx
+++ web/src/components/graph/SystemGraph.tsx
@@ -150,7 +150,7 @@ export function ThresholdBarGraph({
 
 const getUnitSize = (MB: number) => {
   if (isNaN(MB) || MB < 0) return "Invalid number";
-  if (MB < 1024) return `${MB} MiB`;
+  if (MB < 1024) return `${MB.toFixed(2)} MiB`;
   if (MB < 1048576) return `${(MB / 1024).toFixed(2)} GiB`;
 
   return `${(MB / 1048576).toFixed(2)} TiB`;
