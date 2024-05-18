dc177917583d290bb99598df960fb7cf2779cd96
Nicolas Mowen <nickmowen213@gmail.com>
2024-05-18 13:53:39 -0600

    Settings fixes

--- web/src/components/menu/AccountSettings.tsx
+++ web/src/components/menu/AccountSettings.tsx
@@ -34,55 +34,51 @@ export default function AccountSettings({ className }: AccountSettingsProps) {
   const MenuItem = isDesktop ? DropdownMenuItem : DialogClose;
 
   return (
-    <div className={className}>
-      <Container>
-        <Trigger asChild>
-          <a href="#">
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div
-                  className={cn(
-                    "flex flex-col items-center justify-center",
-                    isDesktop
-                      ? "cursor-pointer rounded-lg bg-secondary text-secondary-foreground hover:bg-muted"
-                      : "text-secondary-foreground",
-                    className,
-                  )}
-                >
-                  <VscAccount className="size-5 md:m-[6px]" />
-                </div>
-              </TooltipTrigger>
-              <TooltipPortal>
-                <TooltipContent side="right">
-                  <p>Account</p>
-                </TooltipContent>
-              </TooltipPortal>
-            </Tooltip>
-          </a>
-        </Trigger>
-        <Content
-          className={
-            isDesktop ? "mr-5 w-72" : "max-h-[75dvh] overflow-hidden p-2"
-          }
-        >
-          <div className="w-full flex-col overflow-y-auto overflow-x-hidden">
-            <DropdownMenuLabel>
-              Current User: {profile?.username || "anonymous"}
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator className={isDesktop ? "mt-3" : "mt-1"} />
-            <MenuItem
-              className={
-                isDesktop ? "cursor-pointer" : "flex items-center p-2 text-sm"
-              }
+    <Container>
+      <Trigger asChild>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div
+              className={cn(
+                "flex flex-col items-center justify-center",
+                isDesktop
+                  ? "cursor-pointer rounded-lg bg-secondary text-secondary-foreground hover:bg-muted"
+                  : "text-secondary-foreground",
+                className,
+              )}
             >
-              <a className="flex" href={logoutUrl}>
-                <LuLogOut className="mr-2 size-4" />
-                <span>Logout</span>
-              </a>
-            </MenuItem>
-          </div>
-        </Content>
-      </Container>
-    </div>
+              <VscAccount className="size-5 md:m-[6px]" />
+            </div>
+          </TooltipTrigger>
+          <TooltipPortal>
+            <TooltipContent side="right">
+              <p>Account</p>
+            </TooltipContent>
+          </TooltipPortal>
+        </Tooltip>
+      </Trigger>
+      <Content
+        className={
+          isDesktop ? "mr-5 w-72" : "max-h-[75dvh] overflow-hidden p-2"
+        }
+      >
+        <div className="w-full flex-col overflow-y-auto overflow-x-hidden">
+          <DropdownMenuLabel>
+            Current User: {profile?.username || "anonymous"}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator className={isDesktop ? "mt-3" : "mt-1"} />
+          <MenuItem
+            className={
+              isDesktop ? "cursor-pointer" : "flex items-center p-2 text-sm"
+            }
+          >
+            <a className="flex" href={logoutUrl}>
+              <LuLogOut className="mr-2 size-4" />
+              <span>Logout</span>
+            </a>
+          </MenuItem>
+        </div>
+      </Content>
+    </Container>
   );
 }
