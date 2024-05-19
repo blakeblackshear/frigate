f96828724c0c219a15374d9a3d8be6b163073024
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2024-05-19 13:41:15 -0500

    warn in log if detect fps > 10

--- frigate/config.py
+++ frigate/config.py
@@ -1442,6 +1442,12 @@ class FrigateConfig(FrigateBaseModel):
                         else False
                     )
 
+            # Warn if detect fps > 10
+            if camera_config.detect.fps > 10:
+                logger.warning(
+                    "{camera_config.name} has a detect fps set higher than 10 fps. This does not need to match your camera's fps rate and may cause performance issues. Recommended value is 5 fps."
+                )
+
             # Default min_initialized configuration
             min_initialized = int(camera_config.detect.fps / 2)
             if camera_config.detect.min_initialized is None:
