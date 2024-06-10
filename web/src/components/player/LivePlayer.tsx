aea5ff298624045f88bdc1c4e2df6f604b42dc35
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2024-06-10 17:50:16 -0500

    keep gradients when still image is showing

--- web/src/components/player/LivePlayer.tsx
+++ web/src/components/player/LivePlayer.tsx
@@ -196,7 +196,7 @@ export default function LivePlayer({
       )}
       onClick={onClick}
     >
-      {liveReady && (
+      {((showStillWithoutActivity && !liveReady) || liveReady) && (
         <>
           <div className="pointer-events-none absolute inset-x-0 top-0 z-10 h-[30%] w-full rounded-lg bg-gradient-to-b from-black/20 to-transparent md:rounded-2xl"></div>
           <div className="pointer-events-none absolute inset-x-0 bottom-0 z-10 h-[10%] w-full rounded-lg bg-gradient-to-t from-black/20 to-transparent md:rounded-2xl"></div>
