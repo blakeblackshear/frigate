ce246f6dfc7c8f92654e5ada6960d6d3d1f696b4
Jason Hunter <hunterjm@gmail.com>
2024-06-12 14:48:44 -0400

    manually build pysqlite3 as binaries are only available for x86-64

---
+++ docker/main/build_pysqlite3.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# Fetch the source code for the latest release of Sqlite.
+if [[ ! -d "sqlite" ]]; then
+  wget https://www.sqlite.org/src/tarball/sqlite.tar.gz?r=release -O sqlite.tar.gz
+  tar xzf sqlite.tar.gz
+  cd sqlite/
+  LIBS="-lm" ./configure --disable-tcl --enable-tempstore=always
+  make sqlite3.c
+  cd ../
+  rm sqlite.tar.gz
+fi
+
+# Grab the pysqlite3 source code.
+if [[ ! -d "./pysqlite3" ]]; then
+  git clone https://github.com/coleifer/pysqlite3.git
+fi
+
+# Copy the sqlite3 source amalgamation into the pysqlite3 directory so we can
+# create a self-contained extension module.
+cp "sqlite/sqlite3.c" pysqlite3/
+cp "sqlite/sqlite3.h" pysqlite3/
+
+# Create the wheels and strip symbols to produce manylinux wheels.
+cd pysqlite3/
+sed -i "s|name='pysqlite3-binary'|name=PACKAGE_NAME|g" setup.py
+python3 setup.py build_static
+pip3 wheel . -w /wheels
