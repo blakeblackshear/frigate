e520a20d43a4b8f0a11eeda8313e1c27ba0ee5cd
Jason Hunter <hunterjm@gmail.com>
2024-06-21 17:30:19 -0400

    Semantic Search for Detections (#11899)
    * Initial re-implementation of semantic search
    
    * put docker-compose back and make reindex match docs
    
    * remove debug code and fix import
    
    * fix docs
    
    * manually build pysqlite3 as binaries are only available for x86-64
    
    * update comment in build_pysqlite3.sh
    
    * only embed objects
    
    * better error handling when genai fails
    
    * ask ollama to pull requested model at startup
    
    * update ollama docs
    
    * address some PR review comments
    
    * fix lint
    
    * use IPC to write description, update docs for reindex
    
    * remove gemini-pro-vision from docs as it will be unavailable soon
    
    * fix OpenAI doc available models
    
    * fix api error in gemini and metadata for embeddings

--- frigate/comms/events_updater.py
+++ frigate/comms/events_updater.py
@@ -1,100 +1,51 @@
 """Facilitates communication between processes."""
 
-import zmq
-
 from frigate.events.types import EventStateEnum, EventTypeEnum
 
-SOCKET_PUSH_PULL = "ipc:///tmp/cache/events"
-SOCKET_PUSH_PULL_END = "ipc:///tmp/cache/events_ended"
+from .zmq_proxy import Publisher, Subscriber
 
 
-class EventUpdatePublisher:
+class EventUpdatePublisher(Publisher):
     """Publishes events (objects, audio, manual)."""
 
+    topic_base = "event/"
+
     def __init__(self) -> None:
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PUSH)
-        self.socket.connect(SOCKET_PUSH_PULL)
+        super().__init__("update")
 
     def publish(
         self, payload: tuple[EventTypeEnum, EventStateEnum, str, dict[str, any]]
     ) -> None:
-        """There is no communication back to the processes."""
-        self.socket.send_json(payload)
-
-    def stop(self) -> None:
-        self.socket.close()
-        self.context.destroy()
+        super().publish(payload)
 
 
-class EventUpdateSubscriber:
+class EventUpdateSubscriber(Subscriber):
     """Receives event updates."""
 
+    topic_base = "event/"
+
     def __init__(self) -> None:
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PULL)
-        self.socket.bind(SOCKET_PUSH_PULL)
-
-    def check_for_update(
-        self, timeout=1
-    ) -> tuple[EventTypeEnum, EventStateEnum, str, dict[str, any]]:
-        """Returns events or None if no update."""
-        try:
-            has_update, _, _ = zmq.select([self.socket], [], [], timeout)
-
-            if has_update:
-                return self.socket.recv_json()
-        except zmq.ZMQError:
-            pass
-
-        return None
-
-    def stop(self) -> None:
-        self.socket.close()
-        self.context.destroy()
+        super().__init__("update")
 
 
-class EventEndPublisher:
+class EventEndPublisher(Publisher):
     """Publishes events that have ended."""
 
+    topic_base = "event/"
+
     def __init__(self) -> None:
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PUSH)
-        self.socket.connect(SOCKET_PUSH_PULL_END)
+        super().__init__("finalized")
 
     def publish(
         self, payload: tuple[EventTypeEnum, EventStateEnum, str, dict[str, any]]
     ) -> None:
-        """There is no communication back to the processes."""
-        self.socket.send_json(payload)
-
-    def stop(self) -> None:
-        self.socket.close()
-        self.context.destroy()
+        super().publish(payload)
 
 
-class EventEndSubscriber:
+class EventEndSubscriber(Subscriber):
     """Receives events that have ended."""
 
+    topic_base = "event/"
+
     def __init__(self) -> None:
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PULL)
-        self.socket.bind(SOCKET_PUSH_PULL_END)
-
-    def check_for_update(
-        self, timeout=1
-    ) -> tuple[EventTypeEnum, EventStateEnum, str, dict[str, any]]:
-        """Returns events ended or None if no update."""
-        try:
-            has_update, _, _ = zmq.select([self.socket], [], [], timeout)
-
-            if has_update:
-                return self.socket.recv_json()
-        except zmq.ZMQError:
-            pass
-
-        return None
-
-    def stop(self) -> None:
-        self.socket.close()
-        self.context.destroy()
+        super().__init__("finalized")
