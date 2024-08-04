005c3a7c77618cc639e1b20ae0a206458d519ea4
Nicolas Mowen <nickmowen213@gmail.com>
2024-08-03 18:19:25 -0600

    Add support for mark as reviewed notification action

--- web/public/notifications-worker.js
+++ web/public/notifications-worker.js
@@ -1,10 +1,29 @@
 // Notifications Worker
 
 self.addEventListener("push", function (event) {
+  console.log(`received push at ${new Date()}`)
   // @ts-expect-error we know this exists
   if (event.data) {
     // @ts-expect-error we know this exists
     const data = event.data.json();
+
+    let actions = [];
+
+    switch (data.type ?? "unknown") {
+      case "alert":
+        actions = [
+          {
+            action: "markReviewed",
+            title: "Mark as Reviewed",
+          },
+          {
+            action: "snooze",
+            title: "Snooze",
+          },
+        ];
+        break;
+    }
+
     // @ts-expect-error we know this exists
     self.registration.showNotification(data.title, {
       body: data.message,
@@ -13,6 +32,7 @@ self.addEventListener("push", function (event) {
       badge: "/images/maskable-badge.png",
       tag: data.id,
       data: { id: data.id, link: data.direct_url },
+      actions,
     });
   } else {
     // pass
@@ -26,14 +46,27 @@ self.addEventListener("notificationclick", (event) => {
     // @ts-expect-error we know this exists
     event.notification.close();
 
-    // @ts-expect-error we know this exists
-    if (event.notification.data) {
-      const url = event.notification.data.link;
-      // eslint-disable-next-line no-undef
-      if (clients.openWindow) {
-        // eslint-disable-next-line no-undef
-        return clients.openWindow(url);
-      }
+    switch (event.action ?? "default") {
+      case "markReviewed":
+        if (event.notification.data) {
+          fetch("/api/reviews/viewed", {
+            method: "POST",
+            body: JSON.stringify({ ids: [event.notification.data.id] }),
+          });
+        }
+        break;
+      case "snooze":
+        break;
+      default:
+        // @ts-expect-error we know this exists
+        if (event.notification.data) {
+          const url = event.notification.data.link;
+          // eslint-disable-next-line no-undef
+          if (clients.openWindow) {
+            // eslint-disable-next-line no-undef
+            return clients.openWindow(url);
+          }
+        }
     }
   }
 });
