d0aface6cad288ef60731fdd4efb819d4350df06
Soren L. Hansen <sorenisanerd@gmail.com>
2024-08-09 06:26:26 -0700

    Fix auth when serving Frigate at a subpath (#12815)
    Ensure axios.defaults.baseURL is set when accessing login form.
    
    Drop `/api` prefix in login form's `axios.post` call, since `/api` is
    part of the baseURL.
    
    Redirect to subpath on succesful authentication.
    
    Prepend subpath to default logout url.
    
    Fixes #12814

--- web/src/login.tsx
+++ web/src/login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import LoginPage from "@/pages/LoginPage.tsx";
+import "@/api";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
