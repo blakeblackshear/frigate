d7fae79f5262e680076a084eb7f9993ccc7794bf
Nicolas Mowen <nickmowen213@gmail.com>
2024-08-12 13:31:35 -0600

    Handle empty recordings list case

--- web/src/components/player/dynamic/DynamicVideoPlayer.tsx
+++ web/src/components/player/dynamic/DynamicVideoPlayer.tsx
@@ -167,7 +167,7 @@ export default function DynamicVideoPlayer({
   );
 
   useEffect(() => {
-    if (!controller || !recordings) {
+    if (!controller || !recordings?.length) {
       if (recordings?.length == 0) {
         setNoRecording(true);
       }
