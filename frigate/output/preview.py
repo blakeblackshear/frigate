f2f11af1db98436861729594b334c17501e983cc
Nicolas Mowen <nickmowen213@gmail.com>
2024-08-16 08:49:30 -0600

    Write frames only if recording enabled

--- frigate/output/preview.py
+++ frigate/output/preview.py
@@ -345,8 +345,10 @@ class PreviewRecorder:
             self.output_frames: list[float] = []
 
             # include first frame to ensure consistent duration
-            self.output_frames.append(frame_time)
-            self.write_frame_to_cache(frame_time, frame)
+            if self.config.record.enabled:
+                self.output_frames.append(frame_time)
+                self.write_frame_to_cache(frame_time, frame)
+
             return True
         elif self.should_write_frame(current_tracked_objects, motion_boxes, frame_time):
             self.output_frames.append(frame_time)
