a37beea6d18209b6eafc02b2679dea9d8fd504b8
Nicolas Mowen <nickmowen213@gmail.com>
2024-09-09 08:11:53 -0600

    Formatting

--- frigate/object_processing.py
+++ frigate/object_processing.py
@@ -772,7 +772,11 @@ class CameraState:
 
             # if we don't have access to the current frame or
             # if the object's thumbnail is not from the current frame, skip
-            if not current_frame or obj.false_positive or obj.thumbnail_data["frame_time"] != frame_time:
+            if (
+                not current_frame
+                or obj.false_positive
+                or obj.thumbnail_data["frame_time"] != frame_time
+            ):
                 continue
 
             if object_type in self.best_objects:
