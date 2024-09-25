45b83fe32b02c9a24823b379092b835cf8a9cd58
Nicolas Mowen <nickmowen213@gmail.com>
2024-09-25 09:09:36 -0600

    Remove unused

--- frigate/api/defs/review_query_parameters.py
+++ frigate/api/defs/review_query_parameters.py
@@ -1,7 +1,6 @@
-from datetime import datetime, timedelta
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
 class ReviewQueryParams(BaseModel):
