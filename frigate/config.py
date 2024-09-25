ec625e02305927a5a5a2b131c6d9e9e9547728a5
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2024-09-25 10:33:20 -0500

    object list

--- frigate/config.py
+++ frigate/config.py
@@ -810,8 +810,9 @@ class GenAICameraConfig(BaseModel):
         title="Default caption prompt.",
     )
     object_prompts: Dict[str, str] = Field(default={}, title="Object specific prompts.")
-    objects: Dict[str, int] = Field(
-        default_factory=dict, title="Objects to run generative AI for."
+    objects: Union[str, List[str]] = Field(
+        default_factory=list,
+        title="List of objects to run generative AI for.",
     )
     required_zones: Union[str, List[str]] = Field(
         default_factory=list,
