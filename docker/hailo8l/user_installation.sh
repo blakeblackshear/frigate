20fd1db0f4c8bc3520ad73bb95324f1d6ea3ae7e
Nicolas Mowen <nickmowen213@gmail.com>
2024-09-26 12:16:08 -0600

    Update hailo driver to 4.18 (#13991)

--- docker/hailo8l/user_installation.sh
+++ docker/hailo8l/user_installation.sh
@@ -13,7 +13,7 @@ else
 fi
 
 # Clone the HailoRT driver repository
-git clone --depth 1 --branch v4.17.0 https://github.com/hailo-ai/hailort-drivers.git
+git clone --depth 1 --branch v4.18.0 https://github.com/hailo-ai/hailort-drivers.git
 
 # Build and install the HailoRT driver
 cd hailort-drivers/linux/pcie
@@ -23,10 +23,17 @@ sudo make install
 # Load the Hailo PCI driver
 sudo modprobe hailo_pci
 
+if [ $? -ne 0 ]; then
+  echo "Unable to load hailo_pci module, common reasons for this are:"
+  echo "- Key was rejected by service: Secure Boot is enabling disallowing install."
+  echo "- Permissions are not setup correctly."
+  exit 1
+fi
+
 # Download and install the firmware
 cd ../../
 ./download_firmware.sh
-sudo mv hailo8_fw.4.17.0.bin /lib/firmware/hailo/hailo8_fw.bin
+sudo mv hailo8_fw.4.18.0.bin /lib/firmware/hailo/hailo8_fw.bin
 
 # Install udev rules
 sudo cp ./linux/pcie/51-hailo-udev.rules /etc/udev/rules.d/
