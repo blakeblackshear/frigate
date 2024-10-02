5fc3faff875697cbb94eb73c07218d0f6ee317e7
George Tsiamasiotis <gtsiam@windowslive.com>
2024-10-02 23:42:02 +0300

    Fix logging
    Fixes logging without introducing more junk into FrigateApp.

--- frigate/config/logger.py
+++ frigate/config/logger.py
@@ -1,6 +1,8 @@
+import logging
 from enum import Enum
 
-from pydantic import Field
+from pydantic import Field, ValidationInfo, model_validator
+from typing_extensions import Self
 
 from .base import FrigateBaseModel
 
@@ -20,3 +22,17 @@ class LoggerConfig(FrigateBaseModel):
     logs: dict[str, LogLevel] = Field(
         default_factory=dict, title="Log level for specified processes."
     )
+
+    @model_validator(mode="after")
+    def post_validation(self, info: ValidationInfo) -> Self:
+        if isinstance(info.context, dict) and info.context.get("install", False):
+            logging.getLogger().setLevel(self.default.value.upper())
+
+            log_levels = {
+                "werkzeug": LogLevel.error,
+                "ws4py": LogLevel.error,
+                **self.logs,
+            }
+
+            for log, level in log_levels.items():
+                logging.getLogger(log).setLevel(level.value.upper())
