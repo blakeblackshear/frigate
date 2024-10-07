afac9788cf20483d0f720f72dc17c27461c8e72a
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2024-10-07 11:09:39 -0500

    try build deps

--- docker/main/build_sqlite_vec.sh
+++ docker/main/build_sqlite_vec.sh
@@ -4,6 +4,12 @@ set -euxo pipefail
 
 SQLITE_VEC_VERSION="0.1.3"
 
+cp /etc/apt/sources.list /etc/apt/sources.list.d/sources-src.list
+sed -i 's|deb http|deb-src http|g' /etc/apt/sources.list.d/sources-src.list
+apt-get update
+
+apt-get -yqq build-dep sqlite3 gettext
+
 mkdir /tmp/sqlite_vec
 # Grab the sqlite_vec source code.
 wget -nv https://github.com/asg017/sqlite-vec/archive/refs/tags/v${SQLITE_VEC_VERSION}.tar.gz
@@ -14,4 +20,5 @@ cd /tmp/sqlite_vec/sqlite-vec-${SQLITE_VEC_VERSION}
 make loadable
 
 # install it
+cp dist/vec0 /usr/local/lib
 
