2124a2f1fafcf9b0643fcc2ffdc8ee5d9fd9b442
Nicolas Mowen <nickmowen213@gmail.com>
2024-10-09 15:08:10 -0600

    Don't provide

--- frigate/embeddings/embeddings.py
+++ frigate/embeddings/embeddings.py
@@ -118,7 +118,6 @@ class Embeddings:
             },
             embedding_function=jina_text_embedding_function,
             model_type="text",
-            preferred_providers=["CPUExecutionProvider"],
         )
 
         self.vision_embedding = GenericONNXEmbedding(
@@ -130,7 +129,6 @@ class Embeddings:
             },
             embedding_function=jina_vision_embedding_function,
             model_type="vision",
-            preferred_providers=["CPUExecutionProvider"],
         )
 
     def _create_tables(self):
