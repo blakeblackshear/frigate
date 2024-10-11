f68be070ff77cd246a52659c401e8c8493a3c1bb
Nicolas Mowen <nickmowen213@gmail.com>
2024-10-11 10:30:26 -0600

    Don't fail if zmq is busy

--- frigate/comms/inter_process.py
+++ frigate/comms/inter_process.py
@@ -65,8 +65,11 @@ class InterProcessRequestor:
 
     def send_data(self, topic: str, data: any) -> any:
         """Sends data and then waits for reply."""
-        self.socket.send_json((topic, data))
-        return self.socket.recv_json()
+        try:
+            self.socket.send_json((topic, data))
+            return self.socket.recv_json()
+        except zmq.ZMQError:
+            return ""
 
     def stop(self) -> None:
         self.socket.close()
