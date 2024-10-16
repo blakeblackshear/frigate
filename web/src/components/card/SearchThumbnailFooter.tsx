f6d9e874dc835310180a8164d13fa4e10f65df55
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2024-10-16 11:49:08 -0500

    fix size of activity indicator

--- web/src/components/card/SearchThumbnailFooter.tsx
+++ web/src/components/card/SearchThumbnailFooter.tsx
@@ -118,7 +118,7 @@ export default function SearchThumbnailFooter({
           <TimeAgo time={searchResult.start_time * 1000} dense />
         ) : (
           <div>
-            <ActivityIndicator size={24} />
+            <ActivityIndicator size={16} />
           </div>
         )}
         {formattedDate}
