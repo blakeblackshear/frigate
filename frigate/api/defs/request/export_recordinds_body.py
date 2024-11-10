0027e3fd96a0fb0bb0af59b1fe88e74f6a761bef
Rui Alves <alvesrui951@gmail.com>
2024-11-10 20:44:44 +0000

    Use custom body for the export recordings endpoint

---
+++ frigate/api/defs/request/export_recordinds_body.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from frigate.record.export import PlaybackFactorEnum
+
+
+class ExportRecordingsBody(BaseModel):
+    playback: PlaybackFactorEnum = Field(
+        default=PlaybackFactorEnum.realtime, title="Playback factor"
+    )
+    source: str = "recordings"
+    name: str = Field(title="Friendly name", default=None, min_length=1, max_length=256)
+    image_path: Optional[str] = None
