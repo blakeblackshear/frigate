4212c0297bae0e1881c750622f7ecb8be7411db8
Rui Alves <alvesrui951@gmail.com>
2024-11-10 22:51:53 +0000

    Fix friendly name and source

--- frigate/api/defs/request/export_recordinds_body.py
+++ frigate/api/defs/request/export_recordinds_body.py
@@ -3,13 +3,18 @@ from typing import Union
 from pydantic import BaseModel, Field
 from pydantic.json_schema import SkipJsonSchema
 
-from frigate.record.export import PlaybackFactorEnum
+from frigate.record.export import (
+    PlaybackFactorEnum,
+    PlaybackSourceEnum,
+)
 
 
 class ExportRecordingsBody(BaseModel):
     playback: PlaybackFactorEnum = Field(
         default=PlaybackFactorEnum.realtime, title="Playback factor"
     )
-    source: str = "recordings"
-    name: str = Field(title="Friendly name", min_length=1, max_length=256)
+    source: PlaybackSourceEnum = Field(
+        default=PlaybackSourceEnum.recordings, title="Playback source"
+    )
+    name: str = Field(title="Friendly name", default=None, max_length=256)
     image_path: Union[str, SkipJsonSchema[None]] = None
