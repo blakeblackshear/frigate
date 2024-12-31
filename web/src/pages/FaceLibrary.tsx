c85be4a8e45298771ed38e127111addcd1135cbd
Nicolas Mowen <nickmowen213@gmail.com>
2024-12-31 14:22:34 -0700

    Cleanup

--- web/src/pages/FaceLibrary.tsx
+++ web/src/pages/FaceLibrary.tsx
@@ -304,7 +304,7 @@ function FaceImage({ name, image, onRefresh }: FaceImageProps) {
           });
         }
       });
-  }, [name, image]);
+  }, [name, image, onRefresh]);
 
   return (
     <div
