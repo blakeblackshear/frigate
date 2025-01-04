46cd969a21d891d468a6e85a8069abe2423c2903
Nicolas Mowen <nickmowen213@gmail.com>
2025-01-04 15:21:47 -0600

    Fix facedet download (#15811)
    * Support downloading face models
    
    * Handle download and loading correctly
    
    * Add face dir creation
    
    * Fix error
    
    * Fix
    
    * Formatting
    
    * Move upload to button
    
    * Show number of faces in library for each name
    
    * Add text color for score
    
    * Cleanup

--- frigate/embeddings/embeddings.py
+++ frigate/embeddings/embeddings.py
@@ -123,19 +123,6 @@ class Embeddings:
             device="GPU" if config.semantic_search.model_size == "large" else "CPU",
         )
 
-        if self.config.face_recognition.enabled:
-            self.face_embedding = GenericONNXEmbedding(
-                model_name="facedet",
-                model_file="facedet.onnx",
-                download_urls={
-                    "facedet.onnx": "https://github.com/NickM-27/facenet-onnx/releases/download/v1.0/facedet.onnx",
-                    "landmarkdet.yaml": "https://github.com/NickM-27/facenet-onnx/releases/download/v1.0/landmarkdet.yaml",
-                },
-                model_size="small",
-                model_type=ModelTypeEnum.face,
-                requestor=self.requestor,
-            )
-
         self.lpr_detection_model = None
         self.lpr_classification_model = None
         self.lpr_recognition_model = None
