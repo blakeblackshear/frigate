99c80ff30a09bd6e02f31b719e3a24bdf8c6e2fc
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2025-01-08 10:12:42 -0600

    Only print line and key/value when a line number can be found

--- frigate/__main__.py
+++ frigate/__main__.py
@@ -84,9 +84,10 @@ def main() -> None:
             except Exception as traverse_error:
                 print(f"Could not determine exact line number: {traverse_error}")
 
-            print(f"Line #  : {line_number}")
-            print(f"Key     : {' -> '.join(map(str, error_path))}")
-            print(f"Value   : {error.get('input','-')}")
+            if current != full_config:
+                print(f"Line #  : {line_number}")
+                print(f"Key     : {' -> '.join(map(str, error_path))}")
+                print(f"Value   : {error.get('input','-')}")
             print(f"Message : {error.get('msg', error.get('type', 'Unknown'))}\n")
 
         print("*************************************************************")
