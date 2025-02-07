15472274ee0dc02211c42340ba8034f1194517b8
Nicolas Mowen <nickmowen213@gmail.com>
2025-02-07 13:12:44 -0700

    Update docs sidebar name (#16370)
    * Clarify classification
    
    * Fix face hierarchy as well

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -33,7 +33,7 @@ const sidebars: SidebarsConfig = {
         'configuration/object_detectors',
         'configuration/audio_detectors',
       ],
-      'Semantic Search': [
+      Classifiers: [
         'configuration/semantic_search',
         'configuration/genai',
         'configuration/face_recognition',
