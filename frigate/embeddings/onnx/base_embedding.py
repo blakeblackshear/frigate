f7dfe6317e3478f90d2aa0da9fd0cffb005467e7
Nicolas Mowen <nickmowen213@gmail.com>
2025-02-20 05:54:39 -0700

    Build out base embedding

---
+++ frigate/embeddings/onnx/base_embedding.py
@@ -0,0 +1,36 @@
+"""Base class for onnx embedding implementations."""
+
+from abc import ABC, abstractmethod
+from enum import Enum
+
+from numpy import ndarray
+from PIL import Image
+
+
+class EmbeddingTypeEnum(str, Enum):
+    thumbnail = "thumbnail"
+    description = "description"
+
+
+class BaseEmbedding(ABC):
+    """Base embedding class."""
+
+    @abstractmethod
+    def _load_model_and_utils(self):
+        pass
+
+    @abstractmethod
+    def _preprocess_inputs(self, raw_inputs: any) -> any:
+        pass
+
+    @abstractmethod
+    def run_inference(self, inputs: any) -> list[ndarray]:
+        pass
+
+    @abstractmethod
+    def __call__(
+        self, inputs: list[str] | list[Image.Image] | list[str]
+    ) -> list[ndarray]:
+        self._load_model_and_utils()
+        processed = self._preprocess_inputs(inputs)
+        return self.run_inference(processed)
