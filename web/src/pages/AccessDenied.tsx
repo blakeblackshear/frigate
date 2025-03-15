9eba9ccca20d464d63efc0c404468f7ede30dd50
ZhaiSoul <842607283@qq.com>
2025-03-15 20:50:27 +0800

    fix: fix accessDenied i18n keys error

--- web/src/pages/AccessDenied.tsx
+++ web/src/pages/AccessDenied.tsx
@@ -5,16 +5,16 @@ import { FaExclamationTriangle } from "react-icons/fa";
 
 export default function AccessDenied() {
   useEffect(() => {
-    document.title = t("accessDefined.documentTitle");
+    document.title = t("accessDenied.documentTitle");
   }, []);
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center text-center">
       <FaExclamationTriangle className="mb-4 size-8" />
       <Heading as="h2" className="mb-2">
-        {t("accessDefined.title")}
+        {t("accessDenied.title")}
       </Heading>
-      <p className="text-primary-variant">{t("accessDefined.desc")}</p>
+      <p className="text-primary-variant">{t("accessDenied.desc")}</p>
     </div>
   );
 }
