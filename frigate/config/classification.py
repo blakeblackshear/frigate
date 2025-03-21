3491e2f6dfb57e595780a62d49cb1c57231bb0c3
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2025-03-21 11:29:20 -0500

    docs

--- frigate/config/classification.py
+++ frigate/config/classification.py
@@ -130,6 +130,7 @@ class CameraLicensePlateRecognitionConfig(FrigateBaseModel):
     expire_time: int = Field(
         default=3,
         title="Expire plates not seen after number of seconds (for dedicated LPR cameras only).",
+        gt=0,
     )
     min_area: int = Field(
         default=1000,
