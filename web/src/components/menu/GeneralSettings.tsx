6d4ff66e9f32e1d398828f07697a11f19f74cea6
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2025-04-09 08:38:44 -0500

    add turkish

--- web/src/components/menu/GeneralSettings.tsx
+++ web/src/components/menu/GeneralSettings.tsx
@@ -387,6 +387,24 @@ export default function GeneralSettings({ className }: GeneralSettingsProps) {
                       <span className="ml-6 mr-2">{t("menu.language.pl")}</span>
                     )}
                   </MenuItem>
+                  <MenuItem
+                    className={
+                      isDesktop
+                        ? "cursor-pointer"
+                        : "flex items-center p-2 text-sm"
+                    }
+                    aria-label={t("menu.language.tr")}
+                    onClick={() => setLanguage("es")}
+                  >
+                    {language === "es" ? (
+                      <>
+                        <LuLanguages className="mr-2 size-4" />
+                        {t("menu.language.tr")}
+                      </>
+                    ) : (
+                      <span className="ml-6 mr-2">{t("menu.language.tr")}</span>
+                    )}
+                  </MenuItem>
                 </SubItemContent>
               </Portal>
             </SubItem>
