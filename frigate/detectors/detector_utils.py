a78518bd94a87f2d1c921610bd83209daab00ba1
Jimmy Hon <honyuenkwun@gmail.com>
2025-05-20 04:46:09 +0000

    Refactor common functions for tflite detector implementations

---
+++ frigate/detectors/detector_utils.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def tflite_init(self, interpreter):
+    self.interpreter = interpreter
+
+    self.interpreter.allocate_tensors()
+
+    self.tensor_input_details = self.interpreter.get_input_details()
+    self.tensor_output_details = self.interpreter.get_output_details()
+
+
+def tflite_detect_raw(self, tensor_input):
+    self.interpreter.set_tensor(self.tensor_input_details[0]["index"], tensor_input)
+    self.interpreter.invoke()
+
+    boxes = self.interpreter.tensor(self.tensor_output_details[0]["index"])()[0]
+    class_ids = self.interpreter.tensor(self.tensor_output_details[1]["index"])()[0]
+    scores = self.interpreter.tensor(self.tensor_output_details[2]["index"])()[0]
+    count = int(self.interpreter.tensor(self.tensor_output_details[3]["index"])()[0])
+
+    detections = np.zeros((20, 6), np.float32)
+
+    for i in range(count):
+        if scores[i] < 0.4 or i == 20:
+            break
+        detections[i] = [
+            class_ids[i],
+            float(scores[i]),
+            boxes[i][0],
+            boxes[i][1],
+            boxes[i][2],
+            boxes[i][3],
+        ]
+
+    return detections
