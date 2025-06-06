65ab44163a1dde0c4ef936a1a42f26b72957a597
Nicolas Mowen <nickmowen213@gmail.com>
2025-06-06 09:18:49 -0600

    Formatting

--- frigate/record/maintainer.py
+++ frigate/record/maintainer.py
@@ -578,7 +578,9 @@ class RecordingMaintainer(threading.Thread):
                                 audio_detections,
                             )
                         )
-                elif topic == DetectionTypeEnum.api.value or DetectionTypeEnum.lpr.value:
+                elif (
+                    topic == DetectionTypeEnum.api.value or DetectionTypeEnum.lpr.value
+                ):
                     continue
 
                 if frame_time < run_start - stale_frame_count_threshold:
