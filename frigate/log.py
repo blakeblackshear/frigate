4870fd2f0b8a4ebf3d8aee79b866869874ece331
Nicolas Mowen <nickmowen213@gmail.com>
2025-06-26 07:02:26 -0600

    Cleanup more logs

--- frigate/log.py
+++ frigate/log.py
@@ -193,7 +193,7 @@ class LogRedirect(io.StringIO):
 
 
 @contextmanager
-def redirect_fd_to_queue(queue: Queue[str]) -> Generator[None, None, None]:
+def __redirect_fd_to_queue(queue: Queue[str]) -> Generator[None, None, None]:
     """Redirect file descriptor 1 (stdout) to a pipe and capture output in a queue."""
     stdout_fd = os.dup(1)
     read_fd, write_fd = os.pipe()
@@ -249,7 +249,7 @@ def redirect_output_to_logger(logger: logging.Logger, level: int) -> Any:
 
             try:
                 # Redirect C-level stdout
-                with redirect_fd_to_queue(queue):
+                with __redirect_fd_to_queue(queue):
                     result = func(*args, **kwargs)
             finally:
                 # Restore Python stdout/stderr
