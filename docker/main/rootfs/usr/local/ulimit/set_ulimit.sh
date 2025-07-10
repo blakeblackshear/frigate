213dc97c178bf9f2e39796a7600d5182e22961b6
Nicolas Mowen <nickmowen213@gmail.com>
2025-07-10 08:48:42 -0600

    Set ulimit (#19086)
    * Add script to set ulimits in case they are too low
    
    * Run the script
    
    * Set version
    
    * Set limit if it is too low

---
+++ docker/main/rootfs/usr/local/ulimit/set_ulimit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Newer versions of containerd 2.X+ impose a very low soft file limit of 1024
+# This applies to OSs like HA OS (see https://github.com/home-assistant/operating-system/issues/4110)
+# Attempt to increase this limit
+
+# Get current soft and hard nofile limits
+current_soft_limit=$(ulimit -Sn)
+current_hard_limit=$(ulimit -Hn)
+
+TARGET_SOFT_LIMIT=65536
+TARGET_HARD_LIMIT=65536
+
+if [ "$current_soft_limit" -lt "$TARGET_SOFT_LIMIT" ]; then
+    # Attempt to set both soft and hard limits to the new value
+    # This requires sufficient privileges (e.g., running as root in the container)
+    if ulimit -n "$TARGET_SOFT_LIMIT:$TARGET_HARD_LIMIT"; then
+        new_soft_limit=$(ulimit -Sn)
+        new_hard_limit=$(ulimit -Hn)
+
+        if [ "$new_soft_limit" -ne "$TARGET_SOFT_LIMIT" ] || [ "$new_hard_limit" -ne "$TARGET_HARD_LIMIT" ]; then
+            echo "Warning: Limits were set, but not to the exact target values. Check system constraints."
+        fi
+    else
+        echo "Error: Failed to set new nofile limits."
+    fi
+fi
