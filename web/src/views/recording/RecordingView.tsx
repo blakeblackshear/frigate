0a02c665fcc51ee31de351d5ad3b9670d8bedf26
Nicolas Mowen <nickmowen213@gmail.com>
2025-07-29 09:11:52 -0600

    Improve the tablet layout (#19320)
    * Improve the tablet layout
    
    * Update imports sort
    
    * Fix more imports

--- web/src/views/recording/RecordingView.tsx
+++ web/src/views/recording/RecordingView.tsx
@@ -30,7 +30,12 @@ import {
   useRef,
   useState,
 } from "react";
-import { isDesktop, isMobile } from "react-device-detect";
+import {
+  isDesktop,
+  isMobile,
+  isMobileOnly,
+  isTablet,
+} from "react-device-detect";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
@@ -621,9 +626,16 @@ export function RecordingView({
                     )
                   : cn(
                       "pt-2 portrait:w-full",
-                      mainCameraAspect == "wide"
-                        ? "aspect-wide landscape:w-full"
-                        : "aspect-video landscape:h-[94%] landscape:xl:h-[65%]",
+                      isMobileOnly &&
+                        (mainCameraAspect == "wide"
+                          ? "aspect-wide landscape:w-full"
+                          : "aspect-video landscape:h-[94%] landscape:xl:h-[65%]"),
+                      isTablet &&
+                        (mainCameraAspect == "wide"
+                          ? "aspect-wide landscape:w-full"
+                          : mainCameraAspect == "normal"
+                            ? "landscape:w-full"
+                            : "aspect-video landscape:h-[100%]"),
                     ),
               )}
               style={{
