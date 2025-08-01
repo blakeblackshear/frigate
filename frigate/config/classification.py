0072a88871e94ca4320ef5bed8325a8bce7a0cee
baudneo <86508179+baudneo@users.noreply.github.com>
2025-08-01 17:03:03 -0600

    Update frigate/config/classification.py
    Co-authored-by: Nicolas Mowen <nickmowen213@gmail.com>

--- frigate/config/classification.py
+++ frigate/config/classification.py
@@ -203,8 +203,8 @@ class FaceRecognitionConfig(FrigateBaseModel):
     )
     device: Optional[str] = Field(
         default=None,
-        title="The gpu id to use for face recognition.",
-        description="This is an override, to target a specific gpu use: '0', '1', etc.",
+        title="The device key to use for semantic search.",
+        description="This is an override, to target a specific device. See https://onnxruntime.ai/docs/execution-providers/ for more information",
     )
 
 
