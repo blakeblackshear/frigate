b103ce7e2ef2035779eaef5a67a6b62a556ff8cf
baudneo <86508179+baudneo@users.noreply.github.com>
2025-08-01 17:04:29 -0600

    ArcFace uses GPU by default, no need for 'CPU' value.

--- frigate/embeddings/onnx/face_embedding.py
+++ frigate/embeddings/onnx/face_embedding.py
@@ -151,11 +151,7 @@ class ArcfaceEmbedding(BaseEmbedding):
             self.runner = ONNXModelRunner(
                 os.path.join(self.download_path, self.model_file),
                 device=self.config.face_recognition.device
-                or (
-                    "GPU"
-                    if self.config.face_recognition.model_size == "large"
-                    else "CPU"
-                ),
+                or "GPU",
             )
 
     def _preprocess_inputs(self, raw_inputs):
