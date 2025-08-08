44bc7301929491279034924c3ab9c4f04e35fa61
Nicolas Mowen <nickmowen213@gmail.com>
2025-08-08 13:08:48 -0600

    Automatic migration

--- frigate/config/camera/genai.py
+++ frigate/config/camera/genai.py
@@ -19,7 +19,6 @@ class GenAIProviderEnum(str, Enum):
 class GenAIConfig(FrigateBaseModel):
     """Primary GenAI Config to define GenAI Provider."""
 
-    enabled: bool = Field(default=False, title="Enable GenAI.")
     api_key: Optional[EnvString] = Field(default=None, title="Provider API key.")
     base_url: Optional[str] = Field(default=None, title="Provider base url.")
     model: str = Field(default="gpt-4o", title="GenAI model.")
