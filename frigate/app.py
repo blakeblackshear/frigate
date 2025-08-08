5a1a7441aa13150695d3932f506fbda7552c0142
Nicolas Mowen <nickmowen213@gmail.com>
2025-08-08 13:12:16 -0600

    Always start the embeddings process

--- frigate/app.py
+++ frigate/app.py
@@ -246,18 +246,7 @@ class FrigateApp:
         logger.info(f"Review process started: {review_segment_process.pid}")
 
     def init_embeddings_manager(self) -> None:
-        genai_cameras = [
-            c for c in self.config.cameras.values() if c.enabled and c.genai.enabled
-        ]
-
-        if (
-            not self.config.semantic_search.enabled
-            and not genai_cameras
-            and not self.config.lpr.enabled
-            and not self.config.face_recognition.enabled
-        ):
-            return
-
+        # always start the embeddings process
         embedding_process = EmbeddingProcess(
             self.config, self.embeddings_metrics, self.stop_event
         )
