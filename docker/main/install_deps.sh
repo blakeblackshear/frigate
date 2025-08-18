b45f64286815ba4059c8acac776de8a5f95046da
Nicolas Mowen <nickmowen213@gmail.com>
2025-08-18 07:21:42 -0600

    Use sed on correct file (#19590)

--- docker/main/install_deps.sh
+++ docker/main/install_deps.sh
@@ -58,11 +58,10 @@ fi
 # arch specific packages
 if [[ "${TARGETARCH}" == "amd64" ]]; then
   # Install non-free version of i965 driver
-  CODENAME=$(grep VERSION_CODENAME= /etc/os-release | cut -d= -f2) \
-      && sed -i -E "s/^(deb http:\/\/deb\.debian\.org\/debian ${CODENAME} main)(.*)$/\1 contrib non-free non-free-firmware\2/" /etc/apt/sources.list \
+  sed -i -E "/^Components: main$/s/main/main contrib non-free non-free-firmware/" "/etc/apt/sources.list.d/debian.sources" \
       && apt-get -qq update \
       && apt-get install --no-install-recommends --no-install-suggests -y i965-va-driver-shaders \
-      && sed -i -E "s/(deb http:\/\/deb\.debian\.org\/debian ${CODENAME} main) contrib non-free non-free-firmware/\1/" /etc/apt/sources.list \
+      && sed -i -E "/^Components: main contrib non-free non-free-firmware$/s/main contrib non-free non-free-firmware/main/" "/etc/apt/sources.list.d/debian.sources" \
       && apt-get update
 
     # install amd / intel-i965 driver packages
