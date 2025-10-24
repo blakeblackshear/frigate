49f5d595ea0631742b21e67301830e23cb805868
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2025-10-24 07:50:06 -0500

    Review stream tweaks (#20648)
    * add detail stream selector to mobile drawer
    
    * tweak getDurationFromTimestamps for i18n and abbreviations
    
    * improve lifecycle description labeling
    
    * i18n
    
    * match figma
    
    * fix progress line and add area and ratio tooltip
    
    * allow clicking on chevron without triggering playback
    
    * tweaks
    
    * add key
    
    * change wording
    
    * clean up
    
    * clean up
    
    * remove check
    
    * clean up

--- web/src/components/timeline/EventMenu.tsx
+++ web/src/components/timeline/EventMenu.tsx
@@ -4,19 +4,22 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuPortal,
+  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import { HiDotsHorizontal } from "react-icons/hi";
 import { useApiHost } from "@/api";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import type { Event } from "@/types/event";
-import type { FrigateConfig } from "@/types/frigateConfig";
+import { Event } from "@/types/event";
+import { FrigateConfig } from "@/types/frigateConfig";
 
 type EventMenuProps = {
   event: Event;
   config?: FrigateConfig;
   onOpenUpload?: (e: Event) => void;
   onOpenSimilarity?: (e: Event) => void;
+  selectedObjectId?: string;
+  setSelectedObjectId?: (event: Event | undefined) => void;
 };
 
 export default function EventMenu({
@@ -24,71 +27,87 @@ export default function EventMenu({
   config,
   onOpenUpload,
   onOpenSimilarity,
+  selectedObjectId,
+  setSelectedObjectId,
 }: EventMenuProps) {
   const apiHost = useApiHost();
   const navigate = useNavigate();
   const { t } = useTranslation("views/explore");
 
+  const handleObjectSelect = () => {
+    if (event.id === selectedObjectId) {
+      setSelectedObjectId?.(undefined);
+    } else {
+      setSelectedObjectId?.(event);
+    }
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <button
-          className="mr-2 rounded p-1"
-          aria-label={t("itemMenu.openMenu", { ns: "common" })}
-        >
-          <HiDotsHorizontal className="size-4 text-muted-foreground" />
-        </button>
-      </DropdownMenuTrigger>
-      <DropdownMenuPortal>
-        <DropdownMenuContent>
-          <DropdownMenuItem
-            onSelect={() => {
-              navigate(`/explore?event_id=${event.id}`);
-            }}
-          >
-            {t("details.item.button.viewInExplore")}
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <a
-              download
-              href={
-                event.has_snapshot
-                  ? `${apiHost}api/events/${event.id}/snapshot.jpg`
-                  : `${apiHost}api/events/${event.id}/thumbnail.webp`
-              }
-            >
-              {t("itemMenu.downloadSnapshot.label")}
-            </a>
-          </DropdownMenuItem>
-
-          {event.has_snapshot &&
-            event.plus_id == undefined &&
-            event.data.type == "object" &&
-            config?.plus?.enabled && (
-              <DropdownMenuItem
-                onSelect={() => {
-                  onOpenUpload?.(event);
-                }}
-              >
-                {t("itemMenu.submitToPlus.label")}
-              </DropdownMenuItem>
-            )}
-
-          {event.has_snapshot && config?.semantic_search?.enabled && (
+    <>
+      <span tabIndex={0} className="sr-only" />
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <div className="rounded p-1 pr-2" role="button">
+            <HiDotsHorizontal className="size-4 text-muted-foreground" />
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuPortal>
+          <DropdownMenuContent>
+            <DropdownMenuItem onSelect={handleObjectSelect}>
+              {event.id === selectedObjectId
+                ? t("itemMenu.hideObjectDetails.label")
+                : t("itemMenu.showObjectDetails.label")}
+            </DropdownMenuItem>
+            <DropdownMenuSeparator className="my-0.5" />
             <DropdownMenuItem
               onSelect={() => {
-                if (onOpenSimilarity) onOpenSimilarity(event);
-                else
-                  navigate(
-                    `/explore?search_type=similarity&event_id=${event.id}`,
-                  );
+                navigate(`/explore?event_id=${event.id}`);
               }}
             >
-              {t("itemMenu.findSimilar.label")}
+              {t("details.item.button.viewInExplore")}
             </DropdownMenuItem>
-          )}
-        </DropdownMenuContent>
-      </DropdownMenuPortal>
-    </DropdownMenu>
+            <DropdownMenuItem asChild>
+              <a
+                download
+                href={
+                  event.has_snapshot
+                    ? `${apiHost}api/events/${event.id}/snapshot.jpg`
+                    : `${apiHost}api/events/${event.id}/thumbnail.webp`
+                }
+              >
+                {t("itemMenu.downloadSnapshot.label")}
+              </a>
+            </DropdownMenuItem>
+
+            {event.has_snapshot &&
+              event.plus_id == undefined &&
+              event.data.type == "object" &&
+              config?.plus?.enabled && (
+                <DropdownMenuItem
+                  onSelect={() => {
+                    onOpenUpload?.(event);
+                  }}
+                >
+                  {t("itemMenu.submitToPlus.label")}
+                </DropdownMenuItem>
+              )}
+
+            {event.has_snapshot && config?.semantic_search?.enabled && (
+              <DropdownMenuItem
+                onSelect={() => {
+                  if (onOpenSimilarity) onOpenSimilarity(event);
+                  else
+                    navigate(
+                      `/explore?search_type=similarity&event_id=${event.id}`,
+                    );
+                }}
+              >
+                {t("itemMenu.findSimilar.label")}
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenuPortal>
+      </DropdownMenu>
+    </>
   );
 }
