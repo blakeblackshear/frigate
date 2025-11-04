3af47d63eaa78de972356aca356e5578bf0e93c4
GuoQing Liu <842607283@qq.com>
2025-11-04 22:54:12 +0800

    Update web/src/components/settings/ZoneEditPane.tsx
    Co-authored-by: Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>

--- web/src/components/settings/ZoneEditPane.tsx
+++ web/src/components/settings/ZoneEditPane.tsx
@@ -270,7 +270,7 @@ export default function ZoneEditPane({
     mode: "onBlur",
     defaultValues: {
       name: polygon?.name ?? "",
-      friendly_name: polygon?.friendly_name ?? polygon?.name,
+      friendly_name: polygon?.friendly_name ?? polygon?.name ?? "",
       inertia:
         polygon?.camera &&
         polygon?.name &&
