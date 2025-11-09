a242c31b9e12875b262211ef982dcc2ed75a4bf6
Nicolas Mowen <nickmowen213@gmail.com>
2025-11-09 07:27:33 -0700

    Formatting

--- frigate/data_processing/real_time/custom_classification.py
+++ frigate/data_processing/real_time/custom_classification.py
@@ -227,7 +227,9 @@ class CustomStateClassificationProcessor(RealTimeProcessorApi):
             self.tensor_output_details[0]["index"]
         )[0]
         probs = res / res.sum(axis=0)
-        logger.debug(f"{self.model_config.name} Ran state classification with probabilities: {probs}")
+        logger.debug(
+            f"{self.model_config.name} Ran state classification with probabilities: {probs}"
+        )
         best_id = np.argmax(probs)
         score = round(probs[best_id], 2)
         self.__update_metrics(datetime.datetime.now().timestamp() - now)
@@ -456,7 +458,9 @@ class CustomObjectClassificationProcessor(RealTimeProcessorApi):
             self.tensor_output_details[0]["index"]
         )[0]
         probs = res / res.sum(axis=0)
-        logger.debug(f"{self.model_config.name} Ran object classification with probabilities: {probs}")
+        logger.debug(
+            f"{self.model_config.name} Ran object classification with probabilities: {probs}"
+        )
         best_id = np.argmax(probs)
         score = round(probs[best_id], 2)
         self.__update_metrics(datetime.datetime.now().timestamp() - now)
