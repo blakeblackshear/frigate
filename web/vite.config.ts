779134ed0d0469c40b25d8445537bf2857ec5be8
GuoQing Liu <842607283@qq.com>
2025-11-14 23:26:16 +0800

    Define proxy host variable in vite.config.ts
    Add a new line to define the proxy host variable.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 const proxyHost = process.env.PROXY_HOST || "localhost:5000";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
