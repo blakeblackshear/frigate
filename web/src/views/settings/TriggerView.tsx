9242997079ff9c4fd8e2482b0877312d5a7846f3
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2025-11-23 12:25:00 -0600

    spacing tweaks

--- web/src/views/settings/TriggerView.tsx
+++ web/src/views/settings/TriggerView.tsx
@@ -440,7 +440,7 @@ export default function TriggerView({
   return (
     <div className="flex size-full flex-col md:flex-row">
       <Toaster position="top-center" closeButton={true} />
-      <div className="scrollbar-container order-last mb-10 mt-2 flex h-full w-full flex-col overflow-y-auto pb-2 md:order-none md:mr-3 md:mt-0">
+      <div className="scrollbar-container order-last mb-2 mt-2 flex h-full w-full flex-col overflow-y-auto pb-2 md:order-none md:mr-3 md:mt-0">
         {!isSemanticSearchEnabled ? (
           <div className="mb-5 flex flex-row items-center justify-between gap-2">
             <div className="flex flex-col items-start">
