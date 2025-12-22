304340d047e87732e2d76f85c14f3baef572b94d
Andrew Roberts <adroberts@gmail.com>
2025-12-21 20:46:01 -0500

    fix c/p error

--- frigate/record/export.py
+++ frigate/record/export.py
@@ -225,7 +225,7 @@ class RecordingExporter(threading.Thread):
             ffmpeg_cmd = (
                 parse_preset_hardware_acceleration_encode(
                     self.config.ffmpeg.ffmpeg_path,
-                    self.config.cameras[self.camera].record.export.timelapse_args,
+                    self.config.cameras[self.camera].record.export.hwaccel_args,
                     f"-an {ffmpeg_input}",
                     f"{self.config.cameras[self.camera].record.export.timelapse_args} -movflags +faststart",
                     EncodeTypeEnum.timelapse,
