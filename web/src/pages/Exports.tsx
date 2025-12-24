dd1297b66bdb0380aca2de65e9479e0e4cd8d4b8
Josh Hawkins <32435876+hawkeye217@users.noreply.github.com>
2025-12-24 09:26:20 -0600

    don't flash no exports icon while loading

--- web/src/pages/Exports.tsx
+++ web/src/pages/Exports.tsx
@@ -219,12 +219,12 @@ function Exports() {
               />
             ))}
           </div>
-        ) : (
+        ) : exports !== undefined ? (
           <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center text-center">
             <LuFolderX className="size-16" />
             {t("noExports")}
           </div>
-        )}
+        ) : null}
       </div>
     </div>
   );
