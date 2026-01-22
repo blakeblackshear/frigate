47d2e82d6466310caf72f5418523b3262ec14375
Nicolas Mowen <nickmowen213@gmail.com>
2026-01-22 11:36:55 -0700

    Arcface has compilation error

--- frigate/detectors/detection_runners.py
+++ frigate/detectors/detection_runners.py
@@ -132,7 +132,7 @@ class ONNXModelRunner(BaseModelRunner):
         return model_type in [
             EnrichmentModelTypeEnum.paddleocr.value,
             EnrichmentModelTypeEnum.jina_v2.value,
-            EnrichmentModelTypeEnum.facenet.value,
+            EnrichmentModelTypeEnum.arcface.value,
             ModelTypeEnum.rfdetr.value,
             ModelTypeEnum.dfine.value,
         ]
