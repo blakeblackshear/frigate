746612e8010094e7c046ea2badcaab54d5d940eb
Nicolas Mowen <nickmowen213@gmail.com>
2026-08-11 11:38:27 -0600

    Restore old format

--- docker/main/install_deps.sh
+++ docker/main/install_deps.sh
@@ -12,7 +12,7 @@ apt-get -qq install --no-install-recommends -y \
     lbzip2 \
     procps vainfo \
     unzip locales tzdata libxml2 xz-utils \
-    python3 \
+    python3.13 \
     curl \
     lsof \
     jq \
@@ -22,6 +22,8 @@ apt-get -qq install --no-install-recommends -y \
     libusb-1.0-0 \
     python3-h2 \
     libgomp1  # memryx detector
+
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.13 1
 
 mkdir -p -m 600 /root/.gnupg
 
